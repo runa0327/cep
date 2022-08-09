c0d0af1084aeb3aa39bf5b02d5eb8ffa16762898
qf119ys <qf119ys@163.com>
2022-08-09 22:15:07 +0800

    feat:ORM使用示例

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/PmExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/PmExt.java
@@ -1,12 +1,12 @@
 package com.cisdi.ext.pm;
 
-import com.cisdi.ext.util.JsonUtil;
+import com.cisdi.ext.model.PmPrj;
+import com.cisdi.ext.model.TestStu;
 import com.qygly.ext.jar.helper.ExtJarHelper;
-//import com.qygly.ext.jar.helper.orm.model.TestStu;
 import com.qygly.shared.interaction.EntityRecord;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Date;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -22,16 +22,17 @@ public class PmExt {
     }
 
     private void clonePmPrj(EntityRecord entityRecord) {
-//        TestStu testStu = new TestStu();
-//        testStu.setId("abc").setName("小王").setHeight(1.68d).setBirthDate(new Date()).setAttachmentFile("gogogogo");
-//
-//        String s = JsonUtil.toJson(testStu);
-//        log.info(s);
-//        String csCommId = entityRecord.csCommId;
-//        PmPrj oldPmPrj = PmPrj.selectById(csCommId, Arrays.asList("ID","name","VER"), null);
-//        PmPrj newPmPrj = PmPrj.insertData();
-//        OrmHelper.copyCols(oldPmPrj, newPmPrj, null, Arrays.asList("ID"));
-//        newPmPrj.updateById(null, null, true);
-//        log.info(newPmPrj.toString());
+        String csCommId = entityRecord.csCommId;
+        PmPrj oldPmPrj = PmPrj.selectById(csCommId, null, null);
+        PmPrj newPmPrj = PmPrj.insertData();
+        PmPrj.copyCols(oldPmPrj, newPmPrj, null, Arrays.asList(PmPrj.Cols.ID));
+        newPmPrj.updateById(null, null, false);
+
+        TestStu oldTestStu = TestStu.selectByWhere(null, null, null).get(0);
+        TestStu newTestStu = TestStu.insertData();
+        TestStu.copyCols(oldTestStu, newTestStu, null, Arrays.asList(TestStu.Cols.ID));
+        oldTestStu.deleteById();
+        newTestStu.setName(newTestStu.getName() + "-新");
+        newTestStu.updateById(null, null, true);
     }
 }
