e1ce91b4bd2f20f11135e61f8d0820bb1ed25449
yintao <yintao@ccscit.com>
2022-08-12 11:07:12 +0800

    feat:定时线程处理项目进度计划刷新状态

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/proPlan/ProPlanJob.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/proPlan/ProPlanJob.java
@@ -1,6 +1,6 @@
 package com.cisdi.pms.job.proPlan;
 
-import com.google.common.collect.Lists;
+import com.cisdi.pms.job.utils.ListUtils;
 import com.qygly.ext.rest.helper.feign.client.DataFeignClient;
 import com.qygly.ext.rest.helper.login.keeper.LoginInfoManager;
 import com.qygly.param.data.InvokeActParam;
@@ -10,12 +10,17 @@ import com.qygly.shared.util.JdbcMapUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @Component
 @EnableScheduling
@@ -28,28 +33,41 @@ public class ProPlanJob {
     @Autowired
     DataFeignClient dataFeignClient;
 
-    @Scheduled(
-            fixedDelay = 10000L
-    )
-    public void invokeXxx() {
+    @Resource(name = "taskExecutor")
+    private ThreadPoolTaskExecutor taskExecutor;
 
+    @Scheduled(
+            //一小时执行一次
+            fixedDelay = 60 * 1000L
+    )
+
+    @Async("taskExecutor")
+    public void invokeRefreshProNodeStatus() {
         // 未自动登录前不要执行：
         if (LoginInfoManager.loginInfo == null) {
             return;
         }
 
-        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from PM_WATER_PLAN t limit 1");
-        Map<String, Object> map = list.get(0);
-
-        InvokeActParam invokeActParam = new InvokeActParam();
-        invokeActParam.sevId = "99799190825098168";
-        invokeActParam.actId = "99799190825089165";
-        invokeActParam.isPreChk = false;
-        invokeActParam.idList = Lists.newArrayList(JdbcMapUtil.getString(map, "ID"));
-        invokeActParam.verList = Lists.newArrayList(JdbcMapUtil.getInt(map, "VER"));
-        RespBody<InvokeActResult> respBody = dataFeignClient.invokeAct(invokeActParam);
-        if (respBody.succ) {
-            log.info("成功！");
-        }
+        //没50条开启线程执行
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from pm_prj where `STATUS`='AP'");
+        List<List<Map<String, Object>>> dataList = ListUtils.split(list, 50);
+        AtomicInteger index = new AtomicInteger(0);
+        dataList.forEach(item -> {
+            log.info("定时刷新进度计划状态线程运行--------------------当前进程第" + index.getAndIncrement() + "个");
+            List<String> ids = item.stream().map(m -> JdbcMapUtil.getString(m, "ID")).collect(Collectors.toList());
+            List<Integer> vers = item.stream().map(m -> Integer.parseInt(JdbcMapUtil.getString(m, "VER"))).collect(Collectors.toList());
+            taskExecutor.execute(() -> {
+                InvokeActParam invokeActParam = new InvokeActParam();
+                invokeActParam.sevId = "99799190825090826";
+                invokeActParam.actId = "99902212142027478";
+                invokeActParam.isPreChk = false;
+                invokeActParam.idList = ids;
+                invokeActParam.verList = vers;
+                RespBody<InvokeActResult> respBody = dataFeignClient.invokeAct(invokeActParam);
+                if (respBody.succ) {
+                    log.info("成功！");
+                }
+            });
+        });
     }
 }
