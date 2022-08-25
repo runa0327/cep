a753561be94cc24d2a60cc6cad092843d49aeeff
yintao <yintao@ccscit.com>
2022-08-25 08:52:25 +0800

    feat:项目资料定时刷新文件数量，文件大小

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/file/ProFileExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/file/ProFileExt.java
@@ -3,13 +3,17 @@ package com.cisdi.ext.file;
 import com.cisdi.ext.util.DateTimeUtil;
 import com.cisdi.ext.util.JsonUtil;
 import com.qygly.ext.jar.helper.ExtJarHelper;
+import com.qygly.ext.jar.helper.sql.Crud;
+import com.qygly.shared.interaction.EntityRecord;
 import com.qygly.shared.util.JdbcMapUtil;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.CollectionUtils;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -63,22 +67,24 @@ public class ProFileExt {
         //查询文件夹及其文件
         JdbcTemplate jdbcTemplate = ExtJarHelper.jdbcTemplate.get();
         // TODO 加过滤查询条件
-        List<Map<String, Object>> list = jdbcTemplate.queryForList("select id,name,'' as upload_user,'' as size,'' as upload_time ,''as url,'1' as type from PF_FOLDER where PF_FOLDER_PID = ? \n" +
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select id,name,'' as upload_user,file_size as size,'' as upload_time ,''as url,'1' as type,PF_FOLDER_PID as pid,'' as ext ,file_count from PF_FOLDER where PF_FOLDER_PID = ? \n" +
                 "union all \n" +
-                "select fl.id as id,fl.`NAME`as name,pf.CHIEF_USER_ID as upload_user,SIZE_KB as size,UPLOAD_DTTM as upload_time,FILE_ATTACHMENT_URL as url ,'2' as type\n" +
+                "select fl.id as id,fl.`NAME`as name,pf.CHIEF_USER_ID as upload_user,SIZE_KB as size,UPLOAD_DTTM as upload_time,FILE_ATTACHMENT_URL as url ,'2' as type,'' as pid ,ext,0 as file_count \n" +
                 "from FL_FILE fl \n" +
                 "left join PF_FILE PF on fl.id = pf.FL_FILE_ID \n" +
                 "where pf.PF_FOLDER_ID = ?", folderId, folderId);
         List<FileInfo> fileInfoList = list.stream().map(p -> {
             FileInfo fileInfo = new FileInfo();
             fileInfo.id = JdbcMapUtil.getString(p, "ID");
+            fileInfo.pid = JdbcMapUtil.getString(p, "pid");
             fileInfo.name = JdbcMapUtil.getString(p, "name");
             fileInfo.uploadUser = JdbcMapUtil.getString(p, "upload_user");
-            String uploadTime = JdbcMapUtil.getString(p, "upload_time");
-            fileInfo.uploadTime = "".equals(uploadTime) ? null : DateTimeUtil.stringToTime(uploadTime);
+            fileInfo.uploadTime = JdbcMapUtil.getString(p, "upload_time");
             fileInfo.size = JdbcMapUtil.getString(p, "size");
             fileInfo.url = JdbcMapUtil.getString(p, "url");
             fileInfo.type = JdbcMapUtil.getString(p, "type");
+            fileInfo.ext = JdbcMapUtil.getString(p, "ext");
+            fileInfo.fileCount = JdbcMapUtil.getString(p, "file_count");
             return fileInfo;
         }).collect(Collectors.toList());
         if (CollectionUtils.isEmpty(fileInfoList)) {
@@ -122,11 +128,56 @@ public class ProFileExt {
      */
     public static class FileInfo {
         public String id;
+        public String pid;
         public String name;
         public String uploadUser;
         public String size;
-        public Date uploadTime;
+        public String uploadTime;
         public String url;
         public String type;
+        public String ext;
+        public String fileCount;
+        public String fileSize;
+    }
+
+    public void collectProFileSize() {
+        JdbcTemplate jdbcTemplate = ExtJarHelper.jdbcTemplate.get();
+        List<EntityRecord> entityRecordList = ExtJarHelper.entityRecordList.get();
+        for (EntityRecord entityRecord : entityRecordList) {
+            String csCommId = entityRecord.csCommId;
+
+            //查询文件夹
+            List<Map<String, Object>> folderList = jdbcTemplate.queryForList("select ID,VER,TS,IS_PRESET,CRT_DT,CRT_USER_ID,LAST_MODI_DT,LAST_MODI_USER_ID,STATUS,LK_WF_INST_ID,CODE,NAME,REMARK,PM_PRJ_ID,SEQ_NO,ifnull(PF_FOLDER_PID,'0') as PF_FOLDER_PID,IS_TEMPLATE,FILE_SIZE,FILE_COUNT " +
+                    "from pf_folder where PM_PRJ_ID=?",csCommId);
+            //查询所有文件
+            List<Map<String, Object>> fileList = jdbcTemplate.queryForList("select fl.ID,fl.CODE,fl.NAME,fl.REMARK,fl.VER,fl.FL_PATH_ID,fl.EXT,fl.LK_WF_INST_ID,fl.STATUS,fl.CRT_DT,fl.CRT_USER_ID,fl.LAST_MODI_DT,fl.LAST_MODI_USER_ID,ifnull(SIZE_KB,0) as SIZE_KB,fl.IS_PRESET,fl.FILE_INLINE_URL," +
+                    "fl.FILE_ATTACHMENT_URL,fl.TS,UPLOAD_DTTM,fl.PHYSICAL_LOCATION,fl.DSP_NAME,DSP_SIZE,pff.PF_FOLDER_ID as PF_FOLDER_ID from fl_file fl left join pf_file pff on fl.id = pff.FL_FILE_ID " +
+                    "left join pf_folder pfr on pff.PF_FOLDER_ID = pfr.id where  pfr.PM_PRJ_ID=?",csCommId);
+
+
+            folderList.stream().filter(p -> Objects.equals("0", p.get("PF_FOLDER_PID"))).peek(m -> {
+                List<Map<String, Object>> children = getChildren(m, folderList, fileList);
+                //获取子级的ID集合
+                List<Object> ids = children.stream().map(q -> q.get("ID")).collect(Collectors.toList());
+                ids.add(m.get("ID"));
+                //取出文件
+                List<Map<String, Object>> files = fileList.stream().filter(t -> ids.contains(t.get("PF_FOLDER_ID"))).collect(Collectors.toList());
+                BigDecimal totalSize = files.stream().map(n -> new BigDecimal(String.valueOf(n.get("SIZE_KB")))).reduce(BigDecimal.ZERO, BigDecimal::add);
+                Crud.from("pf_folder").where().eq("ID", m.get("ID")).update().set("FILE_COUNT", children.size()).set("FILE_SIZE", totalSize).exec();
+            }).collect(Collectors.toList());
+        }
+    }
+
+    private List<Map<String, Object>> getChildren(Map<String, Object> root, List<Map<String, Object>> allData, List<Map<String, Object>> fileData) {
+        return allData.stream().filter(p -> Objects.equals(String.valueOf(root.get("ID")), String.valueOf(p.get("PF_FOLDER_PID")))).peek(m -> {
+            List<Map<String, Object>> children = getChildren(m, allData, fileData);
+            //获取子级的ID集合
+            List<Object> ids = children.stream().map(q -> q.get("ID")).collect(Collectors.toList());
+            ids.add(m.get("ID"));
+            //取出文件
+            List<Map<String, Object>> files = fileData.stream().filter(t -> ids.contains(t.get("PF_FOLDER_ID"))).collect(Collectors.toList());
+            BigDecimal totalSize = files.stream().map(n -> new BigDecimal(String.valueOf(n.get("SIZE_KB")))).reduce(BigDecimal.ZERO, BigDecimal::add);
+            Crud.from("pf_folder").where().eq("ID", m.get("ID")).update().set("FILE_COUNT", children.size()).set("FILE_SIZE", totalSize).exec();
+        }).collect(Collectors.toList());
     }
 }
---
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/proFile/ProFileJob.java
@@ -0,0 +1,81 @@
+package com.cisdi.pms.job.proFile;
+
+import com.cisdi.pms.job.utils.ListUtils;
+import com.qygly.ext.rest.helper.feign.client.DataFeignClient;
+import com.qygly.ext.rest.helper.keeper.LoginInfoManager;
+import com.qygly.param.data.InvokeActParam;
+import com.qygly.shared.RespBody;
+import com.qygly.shared.interaction.InvokeActResult;
+import com.qygly.shared.util.JdbcMapUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * @author 尹涛 * @version V1.0.0
+ * @projectName cisdi-pms-service
+ * @title ProFileJob
+ * @package com.cisdi.pms.job.proFile
+ * @description 定时处理文件夹的大小，文件数量
+ * @date 2022/8/23
+ */
+@Component
+@EnableScheduling
+@Slf4j
+public class ProFileJob {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    DataFeignClient dataFeignClient;
+
+    @Resource(name = "taskExecutor")
+    private ThreadPoolTaskExecutor taskExecutor;
+
+    @Scheduled(
+            //一小时执行一次
+            fixedDelay = 60 * 1000L
+    )
+
+    @Async("taskExecutor")
+    public void invokeRefreshProFile() {
+        // 未自动登录前不要执行：
+        if (LoginInfoManager.loginInfo == null) {
+            return;
+        }
+
+        //没50条开启线程执行
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from pm_prj where `STATUS`='AP'");
+        List<List<Map<String, Object>>> dataList = ListUtils.split(list, 50);
+        AtomicInteger index = new AtomicInteger(0);
+        dataList.forEach(item -> {
+            log.info("定时刷新文件夹树线程运行--------------------当前进程第" + index.getAndIncrement() + "个");
+            List<String> ids = item.stream().map(m -> JdbcMapUtil.getString(m, "ID")).collect(Collectors.toList());
+            List<Integer> vers = item.stream().map(m -> Integer.parseInt(JdbcMapUtil.getString(m, "VER"))).collect(Collectors.toList());
+            taskExecutor.execute(() -> {
+                InvokeActParam invokeActParam = new InvokeActParam();
+                invokeActParam.sevId = "99799190825090826";
+                invokeActParam.actId = "99902212142088375";
+                invokeActParam.isPreChk = false;
+                invokeActParam.idList = ids;
+                invokeActParam.verList = vers;
+                RespBody<InvokeActResult> respBody = dataFeignClient.invokeAct(invokeActParam);
+                if (respBody.succ) {
+                    log.info("成功！");
+                }
+            });
+        });
+    }
+}
