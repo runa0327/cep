33cfda66d41f6c9c7389d20d63a633e298626391
LiuXueF <1297157406@qq.com>
2022-08-30 11:07:33 +0800

    fix 流程创建及审批通过相关属性写入及校验

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/wf/WfExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/wf/WfExt.java
@@ -9,7 +9,6 @@ import com.qygly.shared.ad.sev.SevInfo;
 import com.qygly.shared.interaction.EntityRecord;
 import com.qygly.shared.util.SharedUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.connection.ConnectionUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.CollectionUtils;
 
@@ -21,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.prefs.BackingStoreException;
 
 @Slf4j
 public class WfExt {
@@ -65,48 +63,29 @@ public class WfExt {
         if (entityRecordList != null) {
             for (EntityRecord entityRecord : entityRecordList) {
                 String csCommId = entityRecord.csCommId;
-                int update = jdbcTemplate.update("update " + entityCode + " t set t.status = ? where t.id=?",
-                        newStatus, csCommId);
+                Map<String, Object> valueMap = entityRecord.valueMap;
+                int update = jdbcTemplate.update("update " + entityCode + " t set t.status = ? where t.id=?",newStatus, csCommId);
                 log.info("已更新：{}", update);
-
-                if (entityCode.equals("PM_PRJ_REQ")) {
-                    if ("APING".equals(newStatus)) {
-                        // 设置名称：
-                        int update1 = jdbcTemplate.update("update PM_PRJ_REQ t set t.name=t.PRJ_NAME where t.id=?",
-                                csCommId);
-                        log.info("已更新：{}", update1);
-
-                        // 设置流程实例名称：
-                        int update2 = jdbcTemplate.update("update wf_process_instance pi join wf_process p on pi" +
-                                ".WF_PROCESS_ID=p.id join ad_user u on pi.START_USER_ID=u.id join pm_prj_req t on pi" +
-                                ".ENTITY_RECORD_ID=t.id and t.id=? set pi.name=concat( p.name,'-', t.prj_name ,'-',u" +
-                                ".name,'-',pi.START_DATETIME)", csCommId);
-                        log.info("已更新：{}", update2);
-                    }
-
-                    if ("AP".equals(newStatus)) {
-                        Object prj_code = jdbcTemplate.queryForMap("select t.PRJ_CODE from PM_PRJ_REQ t where t.id=?"
-                                , csCommId).get("PRJ_CODE");
-
-                        // 若无项目编号：
-                        if (SharedUtil.isEmptyObject(prj_code)) {
-                            // 获取新的项目编号：
-                            Object new_prj_code = jdbcTemplate.queryForMap("select concat('XM','-',lpad((select count" +
-                                    "(*) from pm_prj_req t),4,0),'-',replace(current_date,'-','')) prj_code").get(
-                                            "prj_code");
-
-                            // 设置项目编号、代码：
-                            int update1 = jdbcTemplate.update("update PM_PRJ_REQ t set t.prj_code=?,t.code=? where t" +
-                                    ".id=?", new_prj_code, new_prj_code, csCommId);
-                            log.info("已更新：{}", update1);
-                        }
-
-                    }
-                }
 
                 //审批流审批通过
                 if ("AP".equals(newStatus)) {
                     Format formatCount = new DecimalFormat("0000");
+
+                    //立项申请生成项目编号
+                    if ("PM_PRJ_REQ".equals(entityCode)){
+                        Object prj_code = jdbcTemplate.queryForMap("select t.PRJ_CODE from PM_PRJ_REQ t where t.id=?", csCommId).get("PRJ_CODE");
+
+                        // 若无项目编号：
+                        if (SharedUtil.isEmptyObject(prj_code)) {
+                            // 获取新的项目编号：
+                            Object new_prj_code = jdbcTemplate.queryForMap("select concat('XM','-',lpad((select count(*) from pm_prj_req t),4,0),'-',replace(current_date,'-','')) prj_code").get("prj_code");
+
+                            // 设置项目编号、代码：
+                            int update1 = jdbcTemplate.update("update PM_PRJ_REQ t set t.prj_code=?,t.code=? where t.id=?", new_prj_code, new_prj_code, csCommId);
+                            log.info("已更新：{}", update1);
+                        }
+                    }
+
                     //合同签订批准后生成合同编号
                     if ("PO_ORDER_REQ".equals(entityCode) || "po_order_req".equals(entityCode)) {
 
@@ -121,7 +100,7 @@ public class WfExt {
 
                         String formatNum = formatCount.format(num);
                         String code = "gc-" + year + "-" + formatNum;
-                        String name = entityRecord.valueMap.get("CONTRACT_NAME").toString();
+                        String name = valueMap.get("CONTRACT_NAME").toString();
                         int update2 = jdbcTemplate.update("update PO_ORDER_REQ set CONTRACT_CODE = ? , NAME = ? where id = ?",
                                 code, name, csCommId);
                     }
@@ -133,7 +112,7 @@ public class WfExt {
                         int num = Integer.valueOf(map.get(0).get("num").toString())+1;
                         String formatNum = formatCount.format(num);
 
-                        String relationContractId = entityRecord.valueMap.get("RELATION_CONTRACT_ID").toString();
+                        String relationContractId = valueMap.get("RELATION_CONTRACT_ID").toString();
                         List<Map<String, Object>> nameMap = jdbcTemplate.queryForList("SELECT CONTRACT_NAME FROM " +
                                 "po_order_req where id = ?", relationContractId);
                         String name = nameMap.get(0).get("CONTRACT_NAME").toString();
@@ -142,49 +121,55 @@ public class WfExt {
                         int update1 = jdbcTemplate.update("update PO_ORDER_SUPPLEMENT_REQ set CONTRACT_NAME = ?  " +
                                 "where id = ? ", contractName ,csCommId);
                     }
-                    //新增保函审批通过时存入保函名称
-                    if ("PO_GUARANTEE_LETTER_REQUIRE_REQ".equals(entityCode)){
-                        //获取到流程名称
-                        String sql1 = "select a.NAME from wf_process_instance a left join PO_GUARANTEE_LETTER_REQUIRE_REQ b on a.id = b.LK_WF_INST_ID where b.id = ?";
-                        List<Map<String,Object>> list = jdbcTemplate.queryForList(sql1,csCommId);
-                        if (CollectionUtils.isEmpty(list)){
-                            throw new BaseException("流程标题不能为空");
+
+                    // 通用方法。流程审批完后将流程名称写入该流程表名称字段
+                    List<String> nameList = getTableList();
+                    if (nameList.contains(entityCode)){
+                        if("PM_PRJ_REQ".equals(entityCode)){
+                            int update1 = jdbcTemplate.update("update PM_PRJ_REQ t set t.name=t.PRJ_NAME where t.id=?",csCommId);
+                        } else {
+                            String sql1 = "select a.NAME from wf_process_instance a left join "+entityCode+" b on a.id = b.LK_WF_INST_ID where b.id = ?";
+                            List<Map<String,Object>> list = jdbcTemplate.queryForList(sql1,csCommId);
+                            if (CollectionUtils.isEmpty(list)){
+                                throw new BaseException("流程标题不能为空");
+                            }
+                            String name = list.get(0).get("NAME").toString();
+                            String sql2 = "update "+entityCode+" set NAME = ? where id = ?";
+                            int update2 = jdbcTemplate.update(sql2,name,csCommId);
                         }
-                        String name = list.get(0).get("NAME").toString();
-                        String sql2 = "update PO_GUARANTEE_LETTER_REQUIRE_REQ set NAME = ? where id = ?";
-                        int update2 = jdbcTemplate.update(sql2,name,csCommId);
+
                     }
 
+                    //资金需求计划申请完成同步数据
+                    if ("PM_FUND_REQUIRE_PLAN_REQ".equals(entityCode)){
+                        PmFundReqPlan pmFundReqPlan = PmFundReqPlan.insertData();
+                        String id = pmFundReqPlan.id;
+                        String pmFundRequirePlanReqId = entityRecord.csCommId;
+                        //项目id
+                        String prjId = valueMap.get("AMOUT_PM_PRJ_ID").toString();
+                        //发起部门id
+                        String hrDeptId = valueMap.get("CRT_DEPT_ID").toString();
+                        //计划需求总金额 待修改
+                        //        String totalAmt = valueMap.get("").toString();
+                        BigDecimal totalAmt = new BigDecimal(10);
+                        //提出时间
+                        String applyTime = valueMap.get("CRT_DT").toString();
+                        //备注
+                        String remark = valueMap.get("REMARK").toString();
+                        //计划名称 待修改
+                        String name = "计划名称待修改";
+
+                        String sql = "update pm_fund_req_plan set NAME = ?,PM_PRJ_ID = ?,HR_DEPT_ID = ?,TOTAL_AMT = ?,APPLY_TIME = ?,PM_FUND_REQUIRE_PLAN_REQ_ID = ?,REMARK = ? where id = ?";
+                        jdbcTemplate.update(sql,name,prjId,hrDeptId,totalAmt,applyTime,pmFundRequirePlanReqId,remark,id);
+                    }
                 }
 
-                //资金需求计划申请完成同步数据
-                if ("PM_FUND_REQUIRE_PLAN_REQ".equals(entityCode)){
-                    PmFundReqPlan pmFundReqPlan = PmFundReqPlan.insertData();
-                    String id = pmFundReqPlan.id;
-                    String pmFundRequirePlanReqId = entityRecord.csCommId;
-                    Map<String, Object> valueMap = entityRecord.valueMap;
-                    //项目id
-                    String prjId = valueMap.get("AMOUT_PM_PRJ_ID").toString();
-                    //发起部门id
-                    String hrDeptId = valueMap.get("CRT_DEPT_ID").toString();
-                    //计划需求总金额 待修改
-                    //        String totalAmt = valueMap.get("").toString();
-                    BigDecimal totalAmt = new BigDecimal(10);
-                    //提出时间
-                    String applyTime = valueMap.get("CRT_DT").toString();
-                    //备注
-                    String remark = valueMap.get("REMARK").toString();
-                    //计划名称 待修改
-                    String name = "计划名称待修改";
-
-                    String sql = "update pm_fund_req_plan set NAME = ?,PM_PRJ_ID = ?,HR_DEPT_ID = ?,TOTAL_AMT = ?,APPLY_TIME = ?,PM_FUND_REQUIRE_PLAN_REQ_ID = ?,REMARK = ? where id = ?";
-                    jdbcTemplate.update(sql,name,prjId,hrDeptId,totalAmt,applyTime,pmFundRequirePlanReqId,remark,id);
-                }
-
-                List<String> tableList = getTableList();
-                if (!CollectionUtils.isEmpty(tableList)) {
-                    if (tableList.contains(entityCode)) {
-                        if ("APING".equals(newStatus)) {
+                //审批流程创建
+                if ("APING".equals(newStatus)) {
+                    List<String> tableList = getTableList();
+                    if (!CollectionUtils.isEmpty(tableList)) {
+                        if (tableList.contains(entityCode)) {
+                            //流程名称按规定创建
                             int update1 = 0;
                             if ("PO_ORDER_PAYMENT_REQ".equals(entityCode) || "PM_FUND_REQUIRE_PLAN_REQ".equals(entityCode) || "PM_DESIGN_ASSIGNMENT_BOOK".equals(entityCode)){
                                 //资金需求计划和付款申请项目\设计任务书名称使用的另外的字段
@@ -194,36 +179,28 @@ public class WfExt {
                                 update1 = jdbcTemplate.update("update wf_process_instance pi join wf_process p on pi" +
                                         ".WF_PROCESS_ID=p.id join ad_user u on pi.START_USER_ID=u.id join " + entityCode + " t on pi.ENTITY_RECORD_ID=t.id join pm_prj prj on t.PM_PRJ_ID=prj.id and t.id=? set pi.name=concat( p.name,'-', prj.name ,'-',u.name,'-',pi.START_DATETIME)", csCommId);
                             }
-
-                            if ("po_public_bid_req".equals(entityCode) || "PO_PUBLIC_BID_REQ".equals(entityCode)) {
-                                //采购招标流程
-                                int update2 = jdbcTemplate.update("update po_public_bid_req set name = (select * from" +
-                                        " (select a.name from wf_process_instance a left join po_public_bid_req b on " +
-                                        "a.id = b.LK_WF_INST_ID where b.id = ?)a) where id = ?", csCommId, csCommId);
-                            }
                             log.info("已更新：{}", update1);
+
+                            //发起人是否存在部门信息校验
+                            try {
+                                String hrDeptId = valueMap.get("CRT_DEPT_ID").toString();
+                                if (SharedUtil.isEmptyString(hrDeptId) || "0".equals(hrDeptId)){
+                                    throw new BaseException("对不起，您尚未有部门信息，不允许进行流程提交");
+                                }
+                            } catch (Exception e){
+                                throw new BaseException("对不起，您尚未有部门信息，不允许进行流程提交");
+                            }
                         }
                     }
                 }
-
-//                if (entityCode.equals("PM_PRJ_INVEST1") || entityCode.equals("PM_PRJ_INVEST2") || entityCode.equals
-//                ("PM_PRJ_INVEST3")) {
-//                    if ("APING".equals(newStatus)) {
-//                        int update1 = jdbcTemplate.update("update wf_process_instance pi join wf_process p on pi
-//                        .WF_PROCESS_ID=p.id join ad_user u on pi.START_USER_ID=u.id join " + entityCode + " t on pi
-//                        .ENTITY_RECORD_ID=t.id join pm_prj prj on t.PM_PRJ_ID=prj.id and t.id=? set pi.name=concat(
-//                        p.name,'-', prj.name ,'-',u.name,'-',pi.START_DATETIME)", csCommId);
-//                        log.info("已更新：{}", update1);
-//                    }
-//                }
-
-
             }
         }
     }
 
+    //流程审批通过后需要更新Name字段的
     private List<String> getTableList() {
         List<String> list = new ArrayList<>();
+        list.add("PM_PRJ_REQ"); //立项申请
         list.add("PM_PRJ_INVEST1"); //可研估算
         list.add("PM_PRJ_INVEST2"); //初设概算
         list.add("PM_PRJ_INVEST3"); //预算财评
