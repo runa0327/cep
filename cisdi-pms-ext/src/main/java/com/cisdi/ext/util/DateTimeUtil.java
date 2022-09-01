e1627d4853c0bde19345e9ca999f726d27bd49a0
yintao <yintao@ccscit.com>
2022-09-01 14:50:49 +0800

    feat:进度计划对比表功能开发

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/util/DateTimeUtil.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/util/DateTimeUtil.java
@@ -197,4 +197,48 @@ public class DateTimeUtil {
     public static int getTwoTimeDays(Date maxDate, Date end) {
         return Math.abs((int) ((maxDate.getTime() - end.getTime()) / (1000 * 3600 * 24)));
     }
+
+    /**
+     * 计算两个日期之间相差的天数
+     *
+     * @param smdate 较小的时间
+     * @param bdate  较大的时间
+     * @return 相差天数
+     * @throws ParseException
+     */
+    public static int daysBetween(Date smdate, Date bdate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        smdate = sdf.parse(sdf.format(smdate));
+        bdate = sdf.parse(sdf.format(bdate));
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(smdate);
+        long time1 = cal.getTimeInMillis();
+        cal.setTime(bdate);
+        long time2 = cal.getTimeInMillis();
+        long between_days = (time2 - time1) / (1000 * 3600 * 24);
+
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+
+    /**
+     * 　　 *字符串的日期格式的计算
+     *
+     */
+    public static int daysBetween(String smdate, String bdate) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(sdf.parse(smdate));
+        long time1 = cal.getTimeInMillis();
+        cal.setTime(sdf.parse(bdate));
+        long time2 = cal.getTimeInMillis();
+        long between_days = (time2 - time1) / (1000 * 3600 * 24);
+
+        return Integer.parseInt(String.valueOf(between_days));
+    }
+
+
+    public static void main(String[] args) throws ParseException{
+        int asd = DateTimeUtil.getTwoTimeDays(DateTimeUtil.stringToDate("2022-09-01"), DateTimeUtil.stringToDate("2022-09-10"));
+        System.out.println(asd);
+    }
 }
