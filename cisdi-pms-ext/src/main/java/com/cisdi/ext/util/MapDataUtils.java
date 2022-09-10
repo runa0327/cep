ff321819fdbc11aaeab42a8f65dbc64c6e96808b
qf119ys <qf119ys@163.com>
2022-09-10 16:25:44 +0800

    feat：用平台提供的extClassLoader来读取jar包里的资源文件

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/util/MapDataUtils.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/util/MapDataUtils.java
@@ -3,10 +3,12 @@ package com.cisdi.ext.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.qygly.ext.jar.helper.ExtJarHelper;
 import com.qygly.shared.BaseException;
 import org.springframework.util.ResourceUtils;
 
 import java.io.*;
+import java.net.URL;
 import java.util.Scanner;
 import java.util.UUID;
 
@@ -15,28 +17,26 @@ import java.util.UUID;
  */
 public class MapDataUtils {
     public static String getInitMapData() {
-        Scanner scanner = null;
-        StringBuilder buffer = new StringBuilder();
         try {
-            File file = ResourceUtils.getFile("classpath:map/echartsData.js");
-            if (!file.exists()) {
-                throw new FileNotFoundException();
+            StringBuilder buffer = new StringBuilder();
+            String resName = "map/echartsData.js";
+            // 部署后，因为不是默认的类加载器加载的，所以要用平台提供的extClassLoader来获取资源：
+            URL url = ExtJarHelper.extClassLoader.get().getResource(resName);
+            // 部署后，该资源文件不是直接存放于文件系统中，而是在jar文件压缩包里，所以要用如下方式读取：
+            InputStream inputStream = url.openStream();
+            int len;
+            byte[] buf = new byte[1024];
+            while ((len = inputStream.read(buf)) != -1) {
+                buffer.append(new String(buf, 0, len));
             }
-            scanner = new Scanner(file, "utf-8");
-            while (scanner.hasNextLine()) {
-                buffer.append(scanner.nextLine());
-            }
-        } catch (Exception e) {
-            // e.printStackTrace();
-            throw new BaseException("获取资源文件map/echartsData.js异常！",e);
-        } finally {
-            if (scanner != null) {
-                scanner.close();
-            }
+            inputStream.close();
+
+            String data = buffer.toString();
+            JSONObject object = JSONObject.parseObject(data);
+            return object.toJSONString();
+        } catch (IOException e) {
+            throw new BaseException("获取资源文件map/echartsData.js异常！", e);
         }
-        String data = buffer.toString();
-        JSONObject object = JSONObject.parseObject(data);
-        return object.toJSONString();
     }
 
     public static String initMapData() {
