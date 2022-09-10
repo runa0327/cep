28a863b49db1b0fdbf2b5cc0005a61399cbbbbbb
qf119ys <qf119ys@163.com>
2022-09-10 10:44:02 +0800

    feat:测试获取资源文件

--- cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/WfProcessController.java
+++ cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/WfProcessController.java
@@ -1,14 +1,19 @@
 package com.cisdi.data.transfer;
 
+import com.qygly.shared.BaseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 @RequestMapping("wfProcess")
 @RestController
@@ -30,6 +35,32 @@ public class WfProcessController {
 
             String wfNodeRoleId = Util.insertData(testJdbcTemplate, "wf_node_role");
             testJdbcTemplate.update("update wf_node_role t set t.WF_NODE_ID=?,t.AD_ROLE_ID='99250247095870406' where t.id=?", wfNodeId, wfNodeRoleId);
+        }
+    }
+
+    /**
+     * http://localhost:11116/wfProcess/testResourceUtil
+     */
+    @GetMapping("testResourceUtil")
+    public void testResourceUtil() {
+        Scanner scanner = null;
+        StringBuilder buffer = new StringBuilder();
+        try {
+            File file = ResourceUtils.getFile("classpath:map/echartsData.js");
+            if (!file.exists()) {
+                throw new FileNotFoundException();
+            }
+            scanner = new Scanner(file, "utf-8");
+            while (scanner.hasNextLine()) {
+                buffer.append(scanner.nextLine());
+            }
+        } catch (Exception e) {
+            // e.printStackTrace();
+            throw new BaseException("获取资源文件map/echartsData.js异常！",e);
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
         }
     }
 }
