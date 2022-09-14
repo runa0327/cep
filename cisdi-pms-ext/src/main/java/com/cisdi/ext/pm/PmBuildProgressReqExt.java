38cc6f906366b242686fd8ddc8284f213dc5b756
LiuXueF <1297157406@qq.com>
2022-09-14 17:54:01 +0800

    feat 施工进度计划扩展

---
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/PmBuildProgressReqExt.java
@@ -0,0 +1,32 @@
+package com.cisdi.ext.pm;
+
+import com.cisdi.ext.util.DateTimeUtil;
+import com.qygly.ext.jar.helper.ExtJarHelper;
+import com.qygly.ext.jar.helper.MyJdbcTemplate;
+import com.qygly.shared.interaction.EntityRecord;
+import com.qygly.shared.util.JdbcMapUtil;
+
+import java.text.ParseException;
+
+/**
+ * 施工进度计划扩展
+ */
+public class PmBuildProgressReqExt {
+
+    public void checkData(){
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        EntityRecord entityRecord = ExtJarHelper.entityRecordList.get().get(0);
+        //计划开工日期
+        String start = JdbcMapUtil.getString(entityRecord.valueMap,"PLAN_START_DATE");
+        //计划完工日期
+        String end = JdbcMapUtil.getString(entityRecord.valueMap,"END_DATETIME");
+        int cha = 0;
+        try {
+            cha = DateTimeUtil.getTwoTimeStringDays(end,start);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        //更新合同工期
+        myJdbcTemplate.update("update PM_BUILD_PROGRESS_REQ set CONTRACT_DAYS = ? where id = ?",cha,entityRecord.csCommId);
+    }
+}
