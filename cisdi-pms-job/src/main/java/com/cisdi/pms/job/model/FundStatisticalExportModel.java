4f4de89de66e946429c29822cf9ee619d969b15b
yintao <yintao@ccscit.com>
2022-09-27 15:16:58 +0800

    feat:资金批复，到位，支付导出功能

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/model/FundStatisticalExportModel.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/model/FundStatisticalExportModel.java
@@ -1,5 +1,6 @@
 package com.cisdi.pms.job.model;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import lombok.Data;
 
 /**
@@ -13,28 +14,39 @@ import lombok.Data;
 @Data
 public class FundStatisticalExportModel {
 
-    //资金大类
+    @ExcelProperty("资金大类")
     public String categoryName;
-    //资金来源
+
+    @ExcelProperty("资金来源")
     public String sourceName;
-    //申报金额
+
+    @ExcelProperty("申报金额")
     public String declaredAmount;
     //批复金额
+    @ExcelProperty("批复金额")
     public String approvedAmount;
     //批复日期
+    @ExcelProperty("批复日期")
     public String approvedDate;
     //累计到位资金
+    @ExcelProperty("累计到位资金")
     public String cumulativeInPaceAmt;
     //累计支付资金
+    @ExcelProperty("累计支付资金")
     public String cumulativePayAmt;
     //到位剩余资金
+    @ExcelProperty("到位剩余资金")
     public String syAmt;
     //未到位资金
+    @ExcelProperty("未到位资金")
     public String unInPlaceAmt;
     //总剩余资金
+    @ExcelProperty("总剩余资金")
     public String totalSyAmt;
     //累计支付率
+    @ExcelProperty("累计支付率")
     public String totalPayRate;
     //备注
+    @ExcelProperty("备注")
     public String remark;
 }
