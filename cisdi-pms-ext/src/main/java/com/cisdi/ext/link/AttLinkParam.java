fabbea04b44f6244c0050f8e66a280a818d9e317
qf119ys <qf119ys@163.com>
2022-10-08 13:55:16 +0800

    feat:AttLinkParam类增加valueMap字段

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/link/AttLinkParam.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/link/AttLinkParam.java
@@ -1,8 +1,12 @@
 package com.cisdi.ext.link;
 
+import java.util.Map;
+
 public class AttLinkParam {
     public String sevId;
     public String id;
     public String attCode;
     public String attValue;
+
+    public Map<String, Object> valueMap;
 }
