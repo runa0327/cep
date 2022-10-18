1d5702c482c9dff6a4c1607de90c6f875155bec4
qf119ys <qf119ys@163.com>
2022-10-18 17:19:30 +0800

    feat:自行编写的Util类的insertData方法，改为平台新版（不用存储过程）

--- cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/Util.java
+++ cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/Util.java
@@ -1,5 +1,8 @@
 package com.cisdi.data.transfer;
 
+import com.qygly.ext.jar.helper.MyJdbcTemplate;
+import com.qygly.shared.BaseException;
+import com.qygly.shared.util.SharedUtil;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.CallableStatementCallback;
 import org.springframework.jdbc.core.CallableStatementCreator;
@@ -8,23 +11,47 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class Util {
-    public static String insertData(JdbcTemplate jdbcTemplate, String entCode) {
-        String newId = (String) (jdbcTemplate).execute(new CallableStatementCreator() {
-            public CallableStatement createCallableStatement(Connection con) throws SQLException {
-                CallableStatement cs = con.prepareCall("{CALL DEF_" + entCode + "_INSERT_DATA(?)}");
-                cs.registerOutParameter(1, 12);
-                return cs;
+    public static String insertData(MyJdbcTemplate myJdbcTemplate, String entCode) {
+        Map<String, Object> map = getEntSql(myJdbcTemplate, entCode);
+        if (SharedUtil.isEmptyObject(map.get("NEW_ID_SQL"))) {
+            throw new BaseException("实体" + entCode + "对应的NEW_ID_SQL不能为空！");
+        } else {
+            String new_id_sql = map.get("NEW_ID_SQL").toString();
+            if (SharedUtil.isEmptyObject(map.get("INSERT_DATA_SQL"))) {
+                throw new BaseException("实体" + entCode + "对应的INSERT_DATA_SQL不能为空！");
+            } else {
+                String insert_data_sql = map.get("INSERT_DATA_SQL").toString();
+                Map<String, Object> newIdMap = myJdbcTemplate.queryForMap(new_id_sql, new Object[0]);
+                String newIdStr = null;
+                Iterator var6 = newIdMap.values().iterator();
+                if (var6.hasNext()) {
+                    Object value = var6.next();
+                    newIdStr = value.toString();
+                }
+
+                if (SharedUtil.isEmptyString(newIdStr)) {
+                    throw new BaseException("实体" + entCode + "新的ID不能为空！");
+                } else {
+                    myJdbcTemplate.update(insert_data_sql, new Object[]{newIdStr});
+                    return newIdStr;
+                }
             }
-        }, new CallableStatementCallback<String>() {
-            public String doInCallableStatement(CallableStatement cs) throws SQLException, DataAccessException {
-                cs.execute();
-                String ret = cs.getString(1);
-                cs.close();
-                return ret;
-            }
-        });
-        return newId;
+        }
+    }
+
+    private static Map<String, Object> getEntSql(MyJdbcTemplate myJdbcTemplate, String entCode) {
+        List<Map<String, Object>> list = myJdbcTemplate.queryForList("select t.NEW_ID_SQL,t.INSERT_DATA_SQL,t.NEW_DATA_SQL from ad_ent_sql t where t.code=?", new Object[]{entCode});
+        if (list.size() > 1) {
+            throw new BaseException("实体" + entCode + "不能对应多条AD_ENT_SQL的记录！");
+        } else if (list.size() == 0) {
+            throw new BaseException("实体" + entCode + "没有对应的AD_ENT_SQL的记录！");
+        } else {
+            return (Map) list.get(0);
+        }
     }
 }
