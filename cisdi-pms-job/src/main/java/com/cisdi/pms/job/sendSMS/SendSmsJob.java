0bf6f36ae32c838c01513e186938a77fbae1a3d5
qf119ys <qf119ys@163.com>
2022-10-20 15:33:29 +0800

    feat:修改SendSmsJob类

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/sendSMS/SendSmsJob.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/sendSMS/SendSmsJob.java
@@ -5,19 +5,11 @@ import com.qygly.ext.rest.helper.feign.client.DataFeignClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.StringUtils;
 
-import javax.annotation.Resource;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
 
 /**
@@ -40,47 +32,50 @@ public class SendSmsJob {
     //@Scheduled(fixedDelayString = "${spring.scheduled.fixedDelayString}")
     public void sendSMS() {
 
-        //查询  处于当前阶段的任务  且  为被关闭的任务
+        // 查询  处于当前阶段的任务  且  为被关闭的任务
         List<RemindLog> result = this.querySMSInfo();
 
-            //发送短信
-            result.forEach(remindLog -> {
-                //开启线程
-                new Thread(() -> {
-                    try {
-                        String lockSql = "update ad_lock t set t.LOCK_EXT_DTTM_ATT01_VAL=now() where t.code='WF_TASK_REMIND_LOCK' and (t.LOCK_EXT_DTTM_ATT01_VAL is null or t.LOCK_EXT_DTTM_ATT01_VAL <= SUBDATE(NOW(),INTERVAL -10 minute))";
-                        int lock = jdbcTemplate.update(lockSql);
+        // 发送短信
+        result.forEach(remindLog -> {
+            // 开启线程
+            new Thread(() -> {
+                try {
+                    String lockSql = "update ad_lock t set t.LOCK_EXT_DTTM_ATT01_VAL=now() where t.code='WF_TASK_REMIND_LOCK' and (t.LOCK_EXT_DTTM_ATT01_VAL is null or t.LOCK_EXT_DTTM_ATT01_VAL <= SUBDATE(NOW(),INTERVAL -10 minute))";
+                    int lock = jdbcTemplate.update(lockSql);
 
-                        if (lock > 0) {
-                            //发送短信
-                            System.out.println("数据输出" + remindLog.toString());
+                    if (lock > 0) {
+                        // 发送短信
+                        System.out.println("数据输出" + remindLog.toString());
 
-                            //记录日志
-                            this.saveOrUpdateLog(remindLog);
-                        }
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        //释放锁
-                        String lockSql = "update ad_lock t set t.LOCK_EXT_DTTM_ATT01_VAL=now() where t.code='WF_TASK_REMIND_LOCK'";
-                        jdbcTemplate.update(lockSql);
+                        // 记录日志
+                        this.insertRemindLog(remindLog);
                     }
-                }).start();
-            });
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    // 释放锁
+                    String lockSql = "update ad_lock t set t.LOCK_EXT_DTTM_ATT01_VAL=now() where t.code='WF_TASK_REMIND_LOCK'";
+                    jdbcTemplate.update(lockSql);
+                }
+            }).start();
+        });
     }
 
-    private void saveOrUpdateLog(RemindLog remindLog) {
-        //提醒文本 默认NOTI
-        String remindText = remindLog.getUserName() + "，您好：流程实例“ " + remindLog.getTaskName() + "”已到您处，知晓即可。注：请登录“工程项目信息系统系统”。";
-        if (remindLog.getTaskType().equals("TODO")) {
-            remindText = remindLog.getUserName() + "，您好：流程实例“ " + remindLog.getTaskName() + "”已到您处，请尽快处理。注：请登录“工程项目信息系统系统”。";
-        }
+    /**
+     * 插入提醒日志。
+     *
+     * @param remindLog
+     */
+    private void insertRemindLog(RemindLog remindLog) {
+        // 提醒文本
+        String part = "TODO".equals(remindLog.getTaskType()) ? "请尽快处理" : "请知晓即可";
+        String remindText = remindLog.getUserName() + "，您好：流程实例“ " + remindLog.getTaskName() + "”已到您处，" + part + "。注：请登录“工程项目信息协同系统”。";
 
-        //查询实体id
+        // 查询实体id
         String selectEntId = "select ID from ad_ent where CODE = ?";
         String entId = jdbcTemplate.queryForObject(selectEntId, String.class, "WF_TASK");
 
-        //每发一次信息就保存一次记录
+        // 每发一次信息就保存一次记录
         String updateSql = "INSERT INTO ad_remind_log ( ID, VER, TS, CRT_DT, CRT_USER_ID, LAST_MODI_DT, LAST_MODI_USER_ID, STATUS, CODE, AD_ENT_ID, ENT_CODE, ENTITY_RECORD_ID, REMIND_USER_ID, REMIND_METHOD, REMIND_TARGET, REMIND_TIME, REMIND_TEXT) VALUES (UUID_SHORT(),'1', now() , now() ,'" + remindLog.getUserId() + "',now(),'" + remindLog.getUserId() + "', 'AP' , 'WF_TASK_REMIND_LOCK' ,'" + entId + "', 'WF_TASK','" + remindLog.getTaskId() + "','" + remindLog.getUserId() + "','SMS','" + remindLog.getUserPhone() + "',now(),'" + remindText + "')";
 
         jdbcTemplate.update(updateSql);
