111fe7cc309ea7b0349b8b777135b75797548fde
sanwu <sanwu9774@163.com>
2022-10-31 11:12:43 +0800

    10/31  资金支付明细

---
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/excel/imports/FundPaymentImportController.java
@@ -0,0 +1,73 @@
+package com.cisdi.pms.job.excel.imports;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.read.listener.PageReadListener;
+import com.cisdi.pms.job.excel.model.FundPaymentImportModel;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+/**
+ * @author hgh
+ * @date 2022/10/31
+ * @apiNote
+ */
+
+@RestController
+@RequestMapping("/paymentImport")
+public class FundPaymentImportController {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void Constructor(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @SneakyThrows(IOException.class)
+    @RequestMapping(value = "/import")
+    public String importData(MultipartFile file) {
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        // 这里每次会读取100条数据 然后返回过来 直接调用使用数据就行
+        EasyExcel.read(file.getInputStream(),
+                FundPaymentImportModel.class,
+                new PageReadListener<FundPaymentImportModel>(dataList -> {
+                    for (FundPaymentImportModel model : dataList) {
+                        // 这里就是你处理代码保存的逻辑了
+                        this.importData(model);
+                    }
+                })).sheet().doRead();
+
+        return "";
+    }
+
+    private void importData(FundPaymentImportModel modelList) {
+
+        //String sql = "insert fund_newly_increased_detail into ID=UUID_SHORT(), VER= '1', TS=NOW(), CRT_DT=NOW(), CRT_USER_ID= ?, LAST_MODI_DT= NOW(), LAST_MODI_USER_ID= ?, STATUS= 'AP', REMARK=?, ACCOUNT_SET=?, PM_PRJ_ID=? , CUSTOMER_UNIT=? , VOUCHER_NUM=?, NPER=?  ";
+        String sql = "insert fund_newly_increased_detail into " +
+                "ID=UUID_SHORT(), " +
+                "VER= '1', " +
+                "TS=NOW(), " +
+                "CRT_DT=NOW(), " +
+                "CRT_USER_ID= ?, " +
+                "LAST_MODI_DT= NOW(), " +
+                "LAST_MODI_USER_ID= ?, " +
+                "STATUS= 'AP', " +
+                "REMARK=?, " +
+                "ACCOUNT_SET=?, " +
+                "PM_PRJ_ID=? , " +
+                "CUSTOMER_UNIT=? , " +
+                "VOUCHER_NUM=?, " +
+                "NPER=?  ";
+
+        jdbcTemplate.update("", modelList);
+
+
+    }
+
+}
---
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/excel/model/FundPaymentImportModel.java
@@ -0,0 +1,64 @@
+package com.cisdi.pms.job.excel.model;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
+import com.alibaba.excel.annotation.write.style.ContentStyle;
+import lombok.Data;
+
+/**
+ * 资金支付明细
+ * @author hgh
+ * @date 2022/10/31
+ * @apiNote
+ */
+
+@Data
+public class FundPaymentImportModel {
+
+    @ExcelProperty("费用大类")
+    @ColumnWidth(value = 30)
+    private String costCategoryId;
+
+    @ExcelProperty("期数")
+    @ColumnWidth(value = 30)
+    private String nper;
+
+    @ExcelProperty("费用明细")
+    @ColumnWidth(value = 30)
+    @ContentStyle(dataFormat = 2)
+    private String feeDetail;
+
+    @ExcelProperty("支付金额")
+    @ColumnWidth(value = 30)
+    @ContentStyle(dataFormat = 2)
+    private String payAmt;
+
+    @ExcelProperty("付款单位")
+    @ColumnWidth(value = 30)
+    private String payUnit;
+
+    @ExcelProperty("收款单位")
+    @ColumnWidth(value = 30)
+    private String payee;
+
+    @ExcelProperty("收款银行")
+    @ColumnWidth(value = 30)
+    private String receiptBank;
+
+    @ExcelProperty("收款账户")
+    @ColumnWidth(value = 30)
+    private String receiptAccount;
+
+    @ExcelProperty("所属账套")
+    @ColumnWidth(value = 30)
+    private String accountSet;
+
+    @ExcelProperty("凭证号")
+    @ColumnWidth(value = 30)
+    private String voucherNum;
+
+    @ExcelProperty("备注")
+    @ColumnWidth(value = 30)
+    private String remarke;
+
+}
