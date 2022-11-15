133fac3ac420aab7ffa308728b461cc8737db562
LiuXueF <1297157406@qq.com>
2022-11-16 00:23:46 +0800

    feat 施工许可属性联动

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/link/AttLinkExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/link/AttLinkExt.java
@@ -104,12 +104,171 @@ public class AttLinkExt {
             return linkCUSTOMER_UNIT_ONE(myJdbcTemplate, attValue, entCode,sevId,param);
         } else if ("PROJECT_SOURCE_TYPE_ID".equals(attCode)){ // 项目来源 属性联动
             return linkPROJECT_SOURCE_TYPE_ID(myJdbcTemplate, attValue, entCode,sevId,param);
-        }else if ("ORDER_DEMAND_TYPE".equals(attCode)){ //合同需求类型
+        } else if ("ORDER_DEMAND_TYPE".equals(attCode)){ //合同需求类型
             return linkORDER_DEMAND_TYPE(myJdbcTemplate,attValue,entCode);
-        } else {
+        } else if ("BUILD_PERMIT_TYPE_ID".equals(attCode)){ //施工许可类型
+            return linkBUILD_PERMIT_TYPE_ID(myJdbcTemplate,attValue,entCode);
+        }  else {
             throw new BaseException("属性联动的参数的attCode为" + attCode + "，不支持！");
         }
 
+    }
+
+    // 施工许可类型 属性联动
+    private AttLinkResult linkBUILD_PERMIT_TYPE_ID(MyJdbcTemplate myJdbcTemplate, String attValue, String entCode) {
+        AttLinkResult attLinkResult = new AttLinkResult();
+        //100031468512110542=主体工程施工许可申请,100031468512110543=基坑及土石方工程施工许可申请
+        if ("PM_CONSTRUCT_PERMIT_REQ".equals(entCode)){ //施工许可
+            // 基坑及土石方工程施工许可申请 组
+            Boolean EARTHWORK_APPLY_DATEChangeToShown = true; //申请时间,默认不显示
+            Boolean COMPL_PLAN_DATE_APPLYChangeToShown = true; //计划完成日期,默认不显示
+            Boolean EARTHWORK_APPLY_USERChangeToShown = true; //申请人,默认不显示
+            Boolean PRJ_REQ_FILEChangeToShown = true; //申请材料,默认不显示
+            Boolean REMARKChangeToShown = true; //备注,默认不显示
+            Boolean EARTHWORK_APPLY_DATEChangeToEditable = false; //申请时间,默认不可改
+            Boolean COMPL_PLAN_DATE_APPLYChangeToEditable = false; //计划完成日期,默认不可改
+            Boolean EARTHWORK_APPLY_USERChangeToEditable = false; //申请人,默认不可改
+            Boolean PRJ_REQ_FILEChangeToEditable = false; //申请材料,默认不可改
+            Boolean REMARKChangeToEditable = false; //备注,默认不可改
+            //主体工程施工许可申请 组
+            Boolean SUBJECT_APPLY_DATEChangeToShown = true; //申请时间,默认不显示
+            Boolean SUBJECT_PLAN_COMPL_DATEChangeToShown = true; //计划完成日期,默认不显示
+            Boolean SUBJECT_APPLY_USERChangeToShown = true; //申请人,默认不显示
+            Boolean KEEP_RECORD_FILEChangeToShown = true; //申请材料,默认不显示
+            Boolean ACT_REMARKChangeToShown = true; //备注,默认不显示
+            Boolean SUBJECT_APPLY_DATEChangeToEditable = false; //申请时间,默认不可改
+            Boolean SUBJECT_PLAN_COMPL_DATEChangeToEditable = false; //计划完成日期,默认不可改
+            Boolean SUBJECT_APPLY_USERChangeToEditable = false; //申请人,默认不可改
+            Boolean KEEP_RECORD_FILEChangeToEditable = false; //申请材料,默认不可改
+            Boolean ACT_REMARKChangeToEditable = false; //备注,默认不可改
+            if ("100031468512110542".equals(attValue)){
+                SUBJECT_APPLY_DATEChangeToShown = true;
+                SUBJECT_PLAN_COMPL_DATEChangeToShown = true;
+                SUBJECT_APPLY_USERChangeToShown = true;
+                KEEP_RECORD_FILEChangeToShown = true;
+                ACT_REMARKChangeToShown = true;
+                SUBJECT_APPLY_DATEChangeToEditable = true;
+                SUBJECT_PLAN_COMPL_DATEChangeToEditable = true;
+                SUBJECT_APPLY_USERChangeToEditable = true;
+                KEEP_RECORD_FILEChangeToEditable = true;
+                ACT_REMARKChangeToEditable = true;
+            } else if ("100031468512110543".equals(attValue)){
+                EARTHWORK_APPLY_DATEChangeToShown = true;
+                COMPL_PLAN_DATE_APPLYChangeToShown = true;
+                EARTHWORK_APPLY_USERChangeToShown = true;
+                PRJ_REQ_FILEChangeToShown = true;
+                REMARKChangeToShown = true;
+                EARTHWORK_APPLY_DATEChangeToEditable = true;
+                COMPL_PLAN_DATE_APPLYChangeToEditable = true;
+                EARTHWORK_APPLY_USERChangeToEditable = true;
+                PRJ_REQ_FILEChangeToEditable = true;
+                REMARKChangeToEditable = true;
+
+            }
+            //申请时间
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.TEXT_LONG;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = EARTHWORK_APPLY_DATEChangeToShown;
+                linkedAtt.changeToEditable = EARTHWORK_APPLY_DATEChangeToEditable;
+//                linkedAtt.changeToEditable = true;
+                attLinkResult.attMap.put("EARTHWORK_APPLY_DATE",linkedAtt);
+            }
+            //计划完成日期
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.DATE;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = COMPL_PLAN_DATE_APPLYChangeToShown;
+                linkedAtt.changeToEditable = COMPL_PLAN_DATE_APPLYChangeToEditable;
+                attLinkResult.attMap.put("COMPL_PLAN_DATE_APPLY",linkedAtt);
+            }
+            //申请人
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.TEXT_LONG;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = EARTHWORK_APPLY_USERChangeToShown;
+                linkedAtt.changeToEditable = EARTHWORK_APPLY_USERChangeToEditable;
+                attLinkResult.attMap.put("EARTHWORK_APPLY_USER",linkedAtt);
+            }
+            //申请材料
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.FILE_GROUP;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = PRJ_REQ_FILEChangeToShown;
+                linkedAtt.changeToEditable = PRJ_REQ_FILEChangeToEditable;
+                attLinkResult.attMap.put("PRJ_REQ_FILE",linkedAtt);
+            }
+            //备注
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.TEXT_LONG;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = REMARKChangeToShown;
+                linkedAtt.changeToEditable = REMARKChangeToEditable;
+                attLinkResult.attMap.put("REMARK",linkedAtt);
+            }
+            //备注
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.TEXT_LONG;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = ACT_REMARKChangeToShown;
+                linkedAtt.changeToEditable = ACT_REMARKChangeToEditable;
+                attLinkResult.attMap.put("ACT_REMARK",linkedAtt);
+            }
+            //申请材料
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.FILE_GROUP;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = KEEP_RECORD_FILEChangeToShown;
+                linkedAtt.changeToEditable = KEEP_RECORD_FILEChangeToEditable;
+                attLinkResult.attMap.put("KEEP_RECORD_FILE",linkedAtt);
+            }
+            //申请人
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.TEXT_LONG;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = SUBJECT_APPLY_USERChangeToShown;
+                linkedAtt.changeToEditable = SUBJECT_APPLY_USERChangeToEditable;
+                attLinkResult.attMap.put("SUBJECT_APPLY_USER",linkedAtt);
+            }
+            //计划完成日期
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.DATE;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = SUBJECT_PLAN_COMPL_DATEChangeToShown;
+                linkedAtt.changeToEditable = SUBJECT_PLAN_COMPL_DATEChangeToEditable;
+                attLinkResult.attMap.put("SUBJECT_PLAN_COMPL_DATE",linkedAtt);
+            }
+            //申请时间
+            {
+                LinkedAtt linkedAtt = new LinkedAtt();
+                linkedAtt.type = AttDataTypeE.DATE;
+                linkedAtt.value = null;
+                linkedAtt.text = null;
+                linkedAtt.changeToShown = SUBJECT_APPLY_DATEChangeToShown;
+                linkedAtt.changeToEditable = SUBJECT_APPLY_DATEChangeToEditable;
+                attLinkResult.attMap.put("SUBJECT_APPLY_DATE",linkedAtt);
+            }
+
+        }
+        return attLinkResult;
     }
 
     private AttLinkResult linkORDER_DEMAND_TYPE(MyJdbcTemplate myJdbcTemplate, String attValue, String entCode) {
