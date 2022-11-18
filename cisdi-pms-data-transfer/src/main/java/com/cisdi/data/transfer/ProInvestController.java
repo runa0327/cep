596c908ea1e098f7a97386d983585f6b8d0d676b
yintao <yintao@ccscit.com>
2022-11-18 17:04:04 +0800

    fix:项目部门人员配置导入,老系统数据同步

--- cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/ProInvestController.java
+++ cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/ProInvestController.java
@@ -7,7 +7,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sun.awt.image.PixelConverter;
 
+import java.math.BigDecimal;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -28,9 +30,13 @@ public class ProInvestController {
     @Qualifier("cpmsJdbcTemplate")
     JdbcTemplate cpmsJdbcTemplate;
 
+//    @Autowired
+//    @Qualifier("testJdbcTemplate")
+//    JdbcTemplate jdbcTemplate;
+
     @Autowired
-    @Qualifier("testJdbcTemplate")
-    JdbcTemplate testJdbcTemplate;
+    @Qualifier("prodJdbcTemplate")
+    JdbcTemplate jdbcTemplate;
 
     /**
      * 同步项目测算数据
@@ -40,11 +46,11 @@ public class ProInvestController {
     @GetMapping("transferData")
     public String transferData() {
         // 清除原有数据
-        testJdbcTemplate.update("delete from PM_INVEST_EST_DTL where CPMS_ID is not null");
+        jdbcTemplate.update("delete from PM_INVEST_EST_DTL where CPMS_ID is not null");
 
-        List<Map<String, Object>> projectList = testJdbcTemplate.queryForList("select * from PM_PRJ where `STATUS` = 'ap'");
+        List<Map<String, Object>> projectList = jdbcTemplate.queryForList("select * from PM_PRJ where `STATUS` = 'ap'");
 
-        List<Map<String, Object>> list = cpmsJdbcTemplate.queryForList("select * from project_budget_cost where del_flag='0'");
+        List<Map<String, Object>> list = cpmsJdbcTemplate.queryForList("select id,budget_type,indicators_name,init_flag,pointer_type,project_id,parent_id,ifnull(amount,0) amount,order_num,ancestors,level,del_flag,create_time,create_by,update_by,update_time,version,approval_time from project_budget_cost where del_flag='0'");
         Map<String, List<Map<String, Object>>> objMap = list.stream().collect(Collectors.groupingBy(p -> String.valueOf(p.get("project_id"))));
         for (String key : objMap.keySet()) {
             String pmPrjId = "";
@@ -54,12 +60,12 @@ public class ProInvestController {
             } else {
                 continue;
             }
-            testJdbcTemplate.update("delete from PM_INVEST_EST where PM_PRJ_ID=?", pmPrjId);
+            jdbcTemplate.update("delete from PM_INVEST_EST where PM_PRJ_ID=?", pmPrjId);
             // 投资测算类型
-            List<Map<String, Object>> typeList = testJdbcTemplate.queryForList("select gsv.* from gr_set_value gsv left join gr_set gs on gsv.GR_SET_ID = gs.ID where gs.`CODE` ='invest_est_type'");
+            List<Map<String, Object>> typeList = jdbcTemplate.queryForList("select gsv.* from gr_set_value gsv left join gr_set gs on gsv.GR_SET_ID = gs.ID where gs.`CODE` ='invest_est_type'");
             for (Map<String, Object> stringObjectMap : typeList) {
-                String investId = Util.insertData(testJdbcTemplate, "PM_INVEST_EST");
-                testJdbcTemplate.update("update PM_INVEST_EST set IS_TEMPLATE='0',PM_PRJ_ID=?,INVEST_EST_TYPE_ID=?,CPMS_ID=?,CPMS_UUID=? where id=?", pmPrjId, stringObjectMap.get("ID"), key, key, investId);
+                String investId = Util.insertData(jdbcTemplate, "PM_INVEST_EST");
+
                 String code = String.valueOf(stringObjectMap.get("CODE"));
                 String str = String.valueOf(Integer.parseInt(code.substring(code.length() - 1)) + 1);
                 // 测算明细
@@ -67,7 +73,7 @@ public class ProInvestController {
                 // CPMS系统中的项目预算数据
                 List<Map<String, Object>> budgetList = detList.stream().filter(m -> Objects.equals(str, m.get("budget_type"))).collect(Collectors.toList());
                 // 查询费用类型
-                List<Map<String, Object>> expTypeList = testJdbcTemplate.queryForList("select ID,VER,TS,IS_PRESET,CRT_DT,CRT_USER_ID,LAST_MODI_DT,LAST_MODI_USER_ID,STATUS,LK_WF_INST_ID,CODE,NAME,REMARK,SEQ_NO,ifnull(PM_EXP_TYPE_PID,0) as PM_EXP_TYPE_PID,CALC_BY_PAYMENT,CALC_BY_PROGRESS from PM_EXP_TYPE");
+                List<Map<String, Object>> expTypeList = jdbcTemplate.queryForList("select ID,VER,TS,IS_PRESET,CRT_DT,CRT_USER_ID,LAST_MODI_DT,LAST_MODI_USER_ID,STATUS,LK_WF_INST_ID,CODE,NAME,REMARK,SEQ_NO,ifnull(PM_EXP_TYPE_PID,0) as PM_EXP_TYPE_PID,CALC_BY_PAYMENT,CALC_BY_PROGRESS from PM_EXP_TYPE");
                 AtomicInteger index = new AtomicInteger(1);
                 budgetList.stream().filter(p -> Objects.equals("0", String.valueOf(p.get("parent_id")))).peek(m -> {
                     String id = getInvestDtlId();
@@ -81,9 +87,11 @@ public class ProInvestController {
                             pmExpTypeId = String.valueOf(optionalMap.get().get("ID"));
                         }
                     }
-                    testJdbcTemplate.update("update PM_INVEST_EST_DTL set `NAME`=?,PM_EXP_TYPE_ID=?,AMT=?,SEQ_NO=?,PM_INVEST_EST_ID=?,CPMS_UUID=?,CPMS_ID=? where ID=?", m.get("indicators_name"), pmExpTypeId, m.get("amount"), index.getAndIncrement(), investId, m.get("id"), m.get("id"), id);
+                    jdbcTemplate.update("update PM_INVEST_EST_DTL set `NAME`=?,PM_EXP_TYPE_ID=?,AMT=?,SEQ_NO=?,PM_INVEST_EST_ID=?,CPMS_UUID=?,CPMS_ID=? where ID=?", m.get("indicators_name"), pmExpTypeId, m.get("amount"), index.getAndIncrement(), investId, m.get("id"), m.get("id"), id);
                     getChildren(m, budgetList, investId, id, expTypeList);
                 }).collect(Collectors.toList());
+                BigDecimal total = budgetList.stream().filter(p -> Objects.equals("0", String.valueOf(p.get("parent_id")))).map(m -> new BigDecimal(String.valueOf(m.get("amount")))).reduce(BigDecimal.ZERO, BigDecimal::add);
+                jdbcTemplate.update("update PM_INVEST_EST set IS_TEMPLATE='0',PM_PRJ_ID=?,INVEST_EST_TYPE_ID=?,CPMS_ID=?,CPMS_UUID=?,PRJ_TOTAL_INVEST=? where id=?", pmPrjId, stringObjectMap.get("ID"), key, key, total, investId);
             }
         }
         return "success";
@@ -113,7 +121,7 @@ public class ProInvestController {
                     pmExpTypeId = String.valueOf(optionalMap.get().get("ID"));
                 }
             }
-            testJdbcTemplate.update("update PM_INVEST_EST_DTL set `NAME`=?,PM_EXP_TYPE_ID=?,AMT=?,SEQ_NO=?,PM_INVEST_EST_ID=?,CPMS_UUID=?,CPMS_ID=?,PM_INVEST_EST_DTL_PID=? where ID=?", m.get("indicators_name"), pmExpTypeId, m.get("amount"), index.getAndIncrement(), investId, m.get("id"), m.get("id"), pid, id);
+            jdbcTemplate.update("update PM_INVEST_EST_DTL set `NAME`=?,PM_EXP_TYPE_ID=?,AMT=?,SEQ_NO=?,PM_INVEST_EST_ID=?,CPMS_UUID=?,CPMS_ID=?,PM_INVEST_EST_DTL_PID=? where ID=?", m.get("indicators_name"), pmExpTypeId, m.get("amount"), index.getAndIncrement(), investId, m.get("id"), m.get("id"), pid, id);
             getChildren(m, allData, investId, id, expTypeList);
         }).collect(Collectors.toList());
     }
@@ -124,6 +132,6 @@ public class ProInvestController {
      * @return
      */
     private String getInvestDtlId() {
-        return Util.insertData(testJdbcTemplate, "PM_INVEST_EST_DTL");
+        return Util.insertData(jdbcTemplate, "PM_INVEST_EST_DTL");
     }
 }
--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/excel/imports/PmDeptUserImportController.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/excel/imports/PmDeptUserImportController.java
@@ -36,32 +36,45 @@ public class PmDeptUserImportController {
 
     @SneakyThrows(IOException.class)
     @RequestMapping("/import")
-    public Map<String, Object> importData(MultipartFile file, String projectId) {
+    public Map<String, Object> importData(MultipartFile file) {
         Map<String, Object> result = new HashMap<>();
         List<String> res = new ArrayList<>();
         List<PmDeptUserModel> dataList = EasyExcelUtil.read(file.getInputStream(), PmDeptUserModel.class);
-        Map<String, List<PmDeptUserModel>> mapData = dataList.stream().collect(Collectors.groupingBy(PmDeptUserModel::getDeptName));
-        List<Map<String, Object>> deptList = jdbcTemplate.queryForList("select * from hr_dept where `status` = 'ap'");
-        List<Map<String, Object>> userList = jdbcTemplate.queryForList("select * from ad_user where `status` = 'ap'");
-        for (String key : mapData.keySet()) {
-            Optional<Map<String, Object>> optional = deptList.stream().filter(p -> Objects.equals(key, JdbcMapUtil.getString(p, "NAME"))).findAny();
-            if (optional.isPresent()) {
-                List<PmDeptUserModel> userModelList = mapData.get(key);
-                List<String> userIds = new ArrayList<>();
-                for (PmDeptUserModel pmDeptUserModel : userModelList) {
-                    Optional<Map<String, Object>> userOptional = userList.stream().filter(m -> Objects.equals(pmDeptUserModel.getUserName(), JdbcMapUtil.getString(m, "NAME"))).findAny();
-                    if (userOptional.isPresent()) {
-                        userIds.add(String.valueOf(userOptional.get().get("ID")));
+        Map<String, List<PmDeptUserModel>> pmMapData = dataList.stream().collect(Collectors.groupingBy(PmDeptUserModel::getProjectName));
+        List<Map<String, Object>> projectList = jdbcTemplate.queryForList("select * from PM_PRJ where `status` = 'ap'");
+        for (String pk : pmMapData.keySet()) {
+            Optional<Map<String, Object>> pjOptional = projectList.stream().filter(q -> Objects.equals(pk, JdbcMapUtil.getString(q, "NAME"))).findAny();
+            if (pjOptional.isPresent()) {
+                List<PmDeptUserModel> ownerData = pmMapData.get(pk);
+                Map<String, List<PmDeptUserModel>> mapData = ownerData.stream().collect(Collectors.groupingBy(PmDeptUserModel::getDeptName));
+                List<Map<String, Object>> deptList = jdbcTemplate.queryForList("select * from hr_dept where `status` = 'ap'");
+                List<Map<String, Object>> userList = jdbcTemplate.queryForList("select * from ad_user where `status` = 'ap'");
+                for (String key : mapData.keySet()) {
+                    Optional<Map<String, Object>> optional = deptList.stream().filter(p -> Objects.equals(key, JdbcMapUtil.getString(p, "NAME"))).findAny();
+                    if (optional.isPresent()) {
+                        List<PmDeptUserModel> userModelList = mapData.get(key);
+                        List<String> userIds = new ArrayList<>();
+                        for (PmDeptUserModel pmDeptUserModel : userModelList) {
+                            Optional<Map<String, Object>> userOptional = userList.stream().filter(m -> Objects.equals(pmDeptUserModel.getUserName(), JdbcMapUtil.getString(m, "NAME"))).findAny();
+                            if (userOptional.isPresent()) {
+                                userIds.add(String.valueOf(userOptional.get().get("ID")));
+                            } else {
+                                res.add("用户名称为：" + pmDeptUserModel.getUserName());
+                            }
+                        }
+                        String projectId = String.valueOf(pjOptional.get().get("ID"));
+                        String id = Util.insertData(jdbcTemplate, "PM_DEPT");
+                        jdbcTemplate.update("update PM_DEPT set PM_PRJ_ID=? ,HR_DEPT_ID=?,USER_IDS=? where id=?", projectId, optional.get().get("ID"), String.join(",", userIds), id);
                     } else {
-                        res.add(pmDeptUserModel.getUserName());
+                        res.add("部门名称为：" + key);
                     }
                 }
-                String id = Util.insertData(jdbcTemplate, "PM_DEPT");
-                jdbcTemplate.update("update PM_DEPT set PM_PRJ_ID=? ,HR_DEPT_ID=?,USER_IDS=? where id=?", projectId, optional.get().get("ID"), String.join(",", userIds), id);
             } else {
-                res.add(key);
+                res.add("项目名称为：" + pk);
             }
+
         }
+
 
         if (CollectionUtils.isEmpty(res)) {
             result.put("code", 200);
@@ -69,7 +82,7 @@ public class PmDeptUserImportController {
             return result;
         } else {
             result.put("code", 500);
-            result.put("message", "部门名称或用户名称为:" + String.join(",", res) + "不存在，未导入！");
+            result.put("message", String.join(",", res) + "不存在，未导入！");
             return result;
         }
     }
