2e13bde8f214b852b4a68769586567859bb992c5
yintao <yintao@ccscit.com>
2022-11-21 16:55:06 +0800

    fix:项目部门人员配置导入

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/excel/imports/PmDeptUserImportController.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/excel/imports/PmDeptUserImportController.java
@@ -43,6 +43,7 @@ public class PmDeptUserImportController {
         List<String> userNames = new ArrayList<>();
         List<String> deptNames = new ArrayList<>();
         List<PmDeptUserModel> dataList = EasyExcelUtil.read(file.getInputStream(), PmDeptUserModel.class);
+        //根据项目分组
         Map<String, List<PmDeptUserModel>> pmMapData = dataList.stream().collect(Collectors.groupingBy(PmDeptUserModel::getProjectName));
         List<Map<String, Object>> projectList = jdbcTemplate.queryForList("select * from PM_PRJ where `status` = 'ap'");
         for (String pk : pmMapData.keySet()) {
@@ -50,10 +51,19 @@ public class PmDeptUserImportController {
             if (pjOptional.isPresent()) {
                 String projectId = String.valueOf(pjOptional.get().get("ID"));
                 List<PmDeptUserModel> ownerData = pmMapData.get(pk);
+                //根据部门分组
                 Map<String, List<PmDeptUserModel>> mapData = ownerData.stream().collect(Collectors.groupingBy(PmDeptUserModel::getDeptName));
                 List<Map<String, Object>> deptList = jdbcTemplate.queryForList("select * from hr_dept where `status` = 'ap'");
                 List<Map<String, Object>> userList = jdbcTemplate.queryForList("select * from ad_user where `status` = 'ap'");
                 for (String key : mapData.keySet()) {
+                    List<Map<String, Object>> oldeList = jdbcTemplate.queryForList("select * from PM_DEPT where PM_PRJ_ID=? and HR_DEPT_ID=?", projectId, key);
+                    String pmDeptId = "";
+                    if (CollectionUtils.isEmpty(oldeList)) {
+                        pmDeptId = Util.insertData(jdbcTemplate, "PM_DEPT");
+                    } else {
+                        pmDeptId = String.valueOf(oldeList.get(0).get("ID"));
+                    }
+
                     Optional<Map<String, Object>> optional = deptList.stream().filter(p -> Objects.equals(key, JdbcMapUtil.getString(p, "NAME"))).findAny();
                     if (optional.isPresent()) {
                         List<PmDeptUserModel> userModelList = mapData.get(key);
@@ -71,8 +81,7 @@ public class PmDeptUserImportController {
                                 userNames.add(pmDeptUserModel.getUserName());
                             }
                         }
-                        String id = Util.insertData(jdbcTemplate, "PM_DEPT");
-                        jdbcTemplate.update("update PM_DEPT set PM_PRJ_ID=? ,HR_DEPT_ID=?,USER_IDS=? where id=?", projectId, optional.get().get("ID"), String.join(",", userIds), id);
+                        jdbcTemplate.update("update PM_DEPT set PM_PRJ_ID=? ,HR_DEPT_ID=?,USER_IDS=? where id=?", projectId, optional.get().get("ID"), String.join(",", userIds), pmDeptId);
                     } else {
                         deptNames.add(key);
                     }
@@ -83,15 +92,18 @@ public class PmDeptUserImportController {
         }
 
         if (!CollectionUtils.isEmpty(projectNames)) {
-            res.add("项目名称为:" + String.join(",", projectNames) + "不存在！");
+            List<String> projectNameList = projectNames.stream().distinct().collect(Collectors.toList());
+            res.add("项目名称为:" + String.join(",", projectNameList) + "不存在！");
         }
 
         if (!CollectionUtils.isEmpty(deptNames)) {
-            res.add("部门名称为:" + String.join(",", deptNames) + "不存在！");
+            List<String> deptNameList = projectNames.stream().distinct().collect(Collectors.toList());
+            res.add("部门名称为:" + String.join(",", deptNameList) + "不存在！");
         }
 
         if (!CollectionUtils.isEmpty(userNames)) {
-            res.add("用户名称为:" + String.join(",", userNames) + "不存在！");
+            List<String> userNameList = projectNames.stream().distinct().collect(Collectors.toList());
+            res.add("用户名称为:" + String.join(",", userNameList) + "不存在！");
         }
 
 
