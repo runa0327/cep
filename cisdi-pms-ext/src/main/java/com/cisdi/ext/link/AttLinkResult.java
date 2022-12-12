03228cc31d062a735db7ab556d611fef1584b3ae
LiuXueF <1297157406@qq.com>
2022-12-12 22:36:23 +0800

    fix

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/link/AttLinkResult.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/link/AttLinkResult.java
@@ -1,24 +1,23 @@
 package com.cisdi.ext.link;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import com.qygly.shared.end.B;
-import com.qygly.shared.end.F;
+import com.qygly.shared.end.Front;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AttLinkResult {
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Map<String, LinkedAtt> attMap = new HashMap<>();
 
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Map<String, Boolean> childCreatable = new HashMap<>();
 
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Map<String, Boolean> childClear = new HashMap<>();
 
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Map<String, List<LinkedRecord>> childData = new HashMap<>();
 
 }
--- cisdi-pms-ext/src/main/java/com/cisdi/ext/link/LinkedAtt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/link/LinkedAtt.java
@@ -3,9 +3,7 @@ package com.cisdi.ext.link;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.qygly.shared.BaseOfBase;
 import com.qygly.shared.ad.att.AttDataTypeE;
-import com.qygly.shared.end.B;
-import com.qygly.shared.end.F;
-import com.qygly.shared.fl.FileInfo;
+import com.qygly.shared.end.Front;
 
 import java.util.List;
 
@@ -14,45 +12,45 @@ public class LinkedAtt extends BaseOfBase {
     /**
      * 属性类型。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public AttDataTypeE type;
 
     /**
      * 属性值。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Object value;
 
     /**
      * 属性文本。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public String text;
 
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public List<LinkedAttFileInfo> fileInfoList;
 
     /**
      * 改变为名称。空不改变。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public String changeToName;
 
     /**
      * 改变为是否显示。空不改变。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Boolean changeToShown;
 
     /**
      * 改变为是否可改。空不改变。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Boolean changeToEditable;
 
     /**
      * 改变为是否必填。空不改变。
      */
-    @JsonView({F.class, B.class})
+    @JsonView({Front.class})
     public Boolean changeToMandatory;
 }
