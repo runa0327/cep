ea34ab8ccfc75ec487c5bcf282b99f477c0611a5
qf119ys <qf119ys@163.com>
2022-12-21 18:14:08 +0800

    feat:简化第三方登录的pom依赖和yml配置

--- cisdi-pms-third-party-login/src/main/java/com/cisdi/pms/ThirdPartyApplication.java
+++ cisdi-pms-third-party-login/src/main/java/com/cisdi/pms/ThirdPartyApplication.java
@@ -2,18 +2,17 @@ package com.cisdi.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * @author 尹涛 * @version V1.0.0
  * @projectName cisdi-pms-service
  * @title ThirdPartyAppllcation
  * @package PACKAGE_NAME
- * @description
+ * @description 未使用数据库，通过exclude = {DataSourceAutoConfiguration.class}不自动装配，避免报错Failed to configure a DataSource
  * @date 2022/12/16
  */
-@EnableFeignClients(basePackages = {"com.qygly.ext.rest.helper"})
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ThirdPartyApplication {
     public static void main(String[] args) {
         SpringApplication.run(ThirdPartyApplication.class, args);
