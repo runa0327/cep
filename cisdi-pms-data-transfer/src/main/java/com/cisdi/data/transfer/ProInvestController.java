0bb55326730949ebdcf2d13486c79655d631b917
LiuXueF <1297157406@qq.com>
2022-12-26 14:23:10 +0800

    fix 合同需求审批非系统属性联动fix

--- cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/ProInvestController.java
+++ cisdi-pms-data-transfer/src/main/java/com/cisdi/data/transfer/ProInvestController.java
@@ -7,7 +7,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sun.awt.image.PixelConverter;
 
 import java.math.BigDecimal;
 import java.util.*;
