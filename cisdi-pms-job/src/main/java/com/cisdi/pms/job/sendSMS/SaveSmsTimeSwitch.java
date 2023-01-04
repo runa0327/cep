b269594de8ab7070bd7783fe9d60f93c8a789252
LiuXueF <1297157406@qq.com>
2023-01-04 17:28:08 +0800

    fix

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/sendSMS/SaveSmsTimeSwitch.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/sendSMS/SaveSmsTimeSwitch.java
@@ -1,14 +1,12 @@
 package com.cisdi.pms.job.sendSMS;
 
 import com.cisdi.pms.job.utils.Util;
-import com.sun.org.apache.bcel.internal.generic.SWITCH;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.io.BufferedReader;
