24573c034e0dfbd9a3a909e5778022cbec1d06d3
LiuXueF <1297157406@qq.com>
2023-02-02 10:08:16 +0800

    fix pdf添加水印

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/TestExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/TestExt.java
@@ -11,8 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 public class TestExt {
@@ -21,6 +19,7 @@ public class TestExt {
     public static String inputx = "C:\\Users\\EDY\\Desktop\\demo\\demo.docx";
     public static String output = "C:\\Users\\EDY\\Desktop\\demo\\demo.pdf";
     public static String outputWaterMark = "C:\\Users\\EDY\\Desktop\\demo\\demoWater.pdf";
+    public static String outputWaterMark1 = "C:\\Users\\EDY\\Desktop\\demo\\demoWater1.pdf";
 
 //    @Resource
 //    private DocumentConverter converter;
@@ -47,59 +46,44 @@ public class TestExt {
         converter.convert(inputFile, outputFile);
 
         //添加水印
-        addFooterAndWater("无痕水印",output,outputWaterMark);
-//        addFooterAndWaterNew1("无痕水印",output,outputWaterMark);
+        addFooterAndWater("无痕水印",output,outputWaterMark1);
+        addFooterAndWaterNew1("三亚崖州湾科技城三角梅种质保存与应用基地",output,outputWaterMark);
 
     }
 
     private void addFooterAndWaterNew1(String watermark, String src, String dest) {
-        Map<String, String> map = new HashMap();
         try {
             log.info("成功调用插入水印方法");
             PdfReader reader = new PdfReader(src);
-            map.put("primary",src);
             log.info("准备插入水印前的文件路径为："+src);
             int n = reader.getNumberOfPages();
             PdfStamper stamper = null;
-            try {
-                stamper = new PdfStamper(reader, new FileOutputStream(dest));
-            } catch (DocumentException e) {
-                e.printStackTrace();
-            }
+            stamper = new PdfStamper(reader, new FileOutputStream(dest));
             log.info("插入水印后的文件路径为："+dest);
-            map.put("show",dest);
-            // transparency
             PdfGState gs1 = new PdfGState();
-            gs1.setFillOpacity(0.4f);
-            // properties
+            gs1.setFillOpacity(0.2f);
             PdfContentByte over;
             Rectangle pagesize;
             float x, y;
-            // loop over every page
             for (int i = 1; i <= n; i++) {
                 pagesize = reader.getPageSizeWithRotation(i);
-//                x = (pagesize.getLeft() + pagesize.getRight()) / 2;
-//                y = (pagesize.getTop() + pagesize.getBottom()) / 2;
-                x = (pagesize.getWidth()/3)*2;
+                x = (pagesize.getWidth()/2);
                 y = (pagesize.getHeight()/2);
                 over = stamper.getOverContent(i);
                 over.saveState();
                 over.setGState(gs1);
-
-//                over.addImage(img, w, 0, 0, h, x - (w / 2), y - (h / 2));
                 over.setFontAndSize(BaseFont.createFont("STSong-Light","UniGB-UCS2-H", BaseFont.NOT_EMBEDDED),30);
-                over.showTextAligned(Element.ALIGN_CENTER , watermark, x, y, 55);
+                over.showTextAligned(Element.ALIGN_CENTER , watermark, x, y, 20);
                 over.restoreState();
 
             }
             log.info("插入水印成功文件页数为"+n);
             stamper.close();
             reader.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
         } catch (IOException ioexception) {
             log.error("文件异常" + ioexception);
-        } catch (DocumentException documentexception) {
-            log.error("文件操作异常" + documentexception);
-
         }
     }
 
@@ -112,6 +96,9 @@ public class TestExt {
             BaseFont base = BaseFont.createFont("STSong-Light","UniGB-UCS2-H", BaseFont.NOT_EMBEDDED);// 使用系统字体
             int total = reader.getNumberOfPages() + 1;
             PdfContentByte under;
+            PdfGState gs = new PdfGState();
+            // 设置透明度为0.2
+            gs.setFillOpacity(0.02f);
             Rectangle pageRect = null;
             for (int i = 1; i < total; i++) {
                 for (int i1 = 1; i1 <= 3; i1++) {
@@ -123,14 +110,12 @@ public class TestExt {
                     under = stamper.getOverContent(i);
                     under.saveState();
                     // set Transparency
-                    PdfGState gs = new PdfGState();
-                    // 设置透明度为0.2
-                    gs.setFillOpacity(0.02f);
+
                     under.setGState(gs);
                     under.restoreState();
                     under.beginText();
                     under.setFontAndSize(BaseFont.createFont("STSong-Light","UniGB-UCS2-H", BaseFont.NOT_EMBEDDED),30);
-//                    under.setColorFill(BaseColor.ORANGE);
+                    under.setColorFill(BaseColor.LIGHT_GRAY);
                     // 水印文字成45度角倾斜
                     under.showTextAligned(Element.ALIGN_CENTER , str, x, y, 55);
                     // 添加水印文字
