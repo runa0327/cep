a8dfc3c0adf7bd788735e3b64aeeb6d7b81eb4d7
LiuXueF <1297157406@qq.com>
2023-02-08 11:41:26 +0800

    feat 意见咨询增加意见附件回显

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/ConsultationReqExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/ConsultationReqExt.java
@@ -3,6 +3,7 @@ package com.cisdi.ext.pm;
 import com.qygly.ext.jar.helper.ExtJarHelper;
 import com.qygly.ext.jar.helper.MyJdbcTemplate;
 import com.qygly.shared.util.JdbcMapUtil;
+import com.qygly.shared.util.SharedUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 
@@ -34,20 +35,32 @@ public class ConsultationReqExt {
         //
         String csCommId = ExtJarHelper.entityRecordList.get().get(0).csCommId;
 
-        String sql = "select CONCAT(c.name,': ',a.USER_COMMENT) as value from wf_task a " +
+        String sql = "select CONCAT(c.name,': ',a.USER_COMMENT) as value,a.USER_ATTACHMENT from wf_task a " +
                 "left join wf_node_instance b on a.WF_NODE_INSTANCE_ID = b.id left join ad_user c on a.ad_user_id = c.id " +
                 "where a.wf_process_instance_id = ? and a.WF_NODE_ID = '1621440689575469056' and b.status = 'ap'";
         List<Map<String,Object>> list = myJdbcTemplate.queryForList(sql,procInstId);
         if (!CollectionUtils.isEmpty(list)){
             StringBuilder sb = new StringBuilder();
+            StringBuilder file = new StringBuilder();
             for (int i = 0; i < list.size(); i++) {
+                String file1 = JdbcMapUtil.getString(list.get(i),"USER_ATTACHMENT");
                 if (i != list.size()-1){
                     sb.append(JdbcMapUtil.getString(list.get(i),"value")).append("\n");
+                    if (!SharedUtil.isEmptyString(file1)){
+                        file.append(file1).append(",");
+                    }
                 } else {
                     sb.append(JdbcMapUtil.getString(list.get(i),"value"));
+                    file.append(file1);
                 }
             }
-            Integer exec = myJdbcTemplate.update("update CONSULTATION_REQ set APPROVAL_COMMENT_TWO = ? where id = ?",sb.toString(),csCommId);
+            StringBuilder updateSql = new StringBuilder("update CONSULTATION_REQ set APPROVAL_COMMENT_TWO = ?");
+            if (file.length()>0){
+                updateSql.append(",FILE_ID_TWO=?");
+            }
+            updateSql.append(" where id = ?");
+
+            Integer exec = myJdbcTemplate.update(updateSql.toString(),sb.toString(),file.toString(),csCommId);
             log.info("已执行：{}",exec);
         }
     }
