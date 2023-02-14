84c43850d01aca7051392045c6cb585985f0d8df
qf119ys <qf119ys@163.com>
2023-02-14 16:49:09 +0800

    feat:按qygly新的接口更新ProFileJob

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/proFile/ProFileJob.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/proFile/ProFileJob.java
@@ -4,12 +4,10 @@ import com.cisdi.pms.job.utils.ListUtils;
 import com.qygly.ext.rest.helper.feign.client.DataFeignClient;
 import com.qygly.ext.rest.helper.keeper.LoginInfoManager;
 import com.qygly.param.data.InvokeActParam;
-import com.qygly.shared.BaseException;
 import com.qygly.shared.RespBody;
-import com.qygly.shared.interaction.EntityRecord;
+import com.qygly.shared.SharedConstants;
 import com.qygly.shared.interaction.InvokeActResult;
 import com.qygly.shared.util.JdbcMapUtil;
-import com.qygly.shared.util.SharedUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,12 +15,9 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -62,17 +57,23 @@ public class ProFileJob {
         List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from pm_prj where `STATUS`='AP'");
         List<List<Map<String, Object>>> dataList = ListUtils.split(list, 50);
         AtomicInteger index = new AtomicInteger(0);
-        dataList.forEach(item -> {
+        dataList.forEach(subList -> {
             log.info("定时刷新文件夹树线程运行--------------------当前进程第" + index.getAndIncrement() + "个");
-            List<String> ids = item.stream().map(m -> JdbcMapUtil.getString(m, "ID")).collect(Collectors.toList());
-            List<Integer> vers = item.stream().map(m -> Integer.parseInt(JdbcMapUtil.getString(m, "VER"))).collect(Collectors.toList());
+            // List<String> ids = subList.stream().map(m -> JdbcMapUtil.getString(m, "ID")).collect(Collectors.toList());
+            // List<Integer> vers = subList.stream().map(m -> Integer.parseInt(JdbcMapUtil.getString(m, "VER"))).collect(Collectors.toList());
             taskExecutor.execute(() -> {
                 InvokeActParam invokeActParam = new InvokeActParam();
                 invokeActParam.sevId = "0099799190825090826";
                 invokeActParam.actId = "0099902212142088375";
                 invokeActParam.isPreChk = false;
-                invokeActParam.idList = ids;
-                invokeActParam.verList = vers;
+                // invokeActParam.idList = ids;
+                // invokeActParam.verList = vers;
+                invokeActParam.valueMapList = subList.stream().map(item -> {
+                    HashMap<String, Object> hashMap = new HashMap<>(2);
+                    hashMap.put(SharedConstants.Cols.ID, JdbcMapUtil.getString(item, "ID"));
+                    hashMap.put(SharedConstants.Cols.VER, JdbcMapUtil.getInt(item, "VER"));
+                    return hashMap;
+                }).collect(Collectors.toList());
                 RespBody<InvokeActResult> respBody = dataFeignClient.invokeAct(invokeActParam);
                 if (respBody.succ) {
                     log.info("成功！");
