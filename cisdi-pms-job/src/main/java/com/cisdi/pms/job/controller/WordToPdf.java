210be99d59dafee0a50d09b4d02ef1a7df6a4727
LiuXueF <1297157406@qq.com>
2023-02-21 11:13:12 +0800

    feat word转pdf任务转移至job

---
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/controller/WordToPdf.java
@@ -0,0 +1,25 @@
+package com.cisdi.pms.job.controller;
+
+import com.cisdi.pms.job.domain.PoOrderReq;
+import com.cisdi.pms.job.service.WordToPdfService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping(value = "/toPdf")
+public class WordToPdf {
+
+    @Resource
+    private WordToPdfService wordToPdfService;
+
+    @PostMapping(value = "/wordToPdf")
+    public void wordToPdf(@RequestBody PoOrderReq poOrderReq){
+        wordToPdfService.wordToPdf(poOrderReq);
+    }
+
+    @GetMapping(value = "/test")
+    public String TestHello(){
+        return "HELLO WORLD";
+    }
+}
