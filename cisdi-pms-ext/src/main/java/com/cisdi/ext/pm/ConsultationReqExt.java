8ace14a4c55668d23d7ab95ba10c02cef6a71800
LiuXueF <1297157406@qq.com>
2023-02-21 17:26:44 +0800

    feat 流程获取意见通用方法

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/ConsultationReqExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/ConsultationReqExt.java
@@ -2,11 +2,13 @@ package com.cisdi.ext.pm;
 
 import com.qygly.ext.jar.helper.ExtJarHelper;
 import com.qygly.ext.jar.helper.MyJdbcTemplate;
+import com.qygly.shared.interaction.EntityRecord;
 import com.qygly.shared.util.JdbcMapUtil;
 import com.qygly.shared.util.SharedUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -71,4 +73,49 @@ public class ConsultationReqExt {
             log.info("已执行：{}",exec);
         }
     }
+
+    /**
+     * 意见征询-批准按钮
+     */
+    public void consultationCheck(){
+        String status = "checkOk";
+        processHandle(status);
+    }
+
+    /**
+     * 意见征询-拒绝按钮
+     */
+    public void consultationCheckRefuse(){
+        String status = "checkRefuse";
+        processHandle(status);
+    }
+
+    /**
+     * 流程流转逻辑处理
+     * @param status 状态码
+     */
+    private void processHandle(String status) {
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        EntityRecord entityRecord = ExtJarHelper.entityRecordList.get().get(0);
+        // 流程实例id
+        String procInstId = ExtJarHelper.procInstId.get();
+        // 流程业务表id
+        String csCommId = entityRecord.csCommId;
+        //当前操作人
+        String userName = ExtJarHelper.loginInfo.get().userName;
+        String userId = ExtJarHelper.loginInfo.get().userId;
+        //获取提交产生的附件和意见信息
+        Map<String,String> map = ProcessCommon.getComment(procInstId,userId,myJdbcTemplate);
+        String comment = map.get("comment");
+        String file = "";
+        if (map.containsKey("file")){
+            file = map.get("file");
+        }
+        //获取流程中的附件和意见信息
+        String processComment = JdbcMapUtil.getString(entityRecord.valueMap,"APPROVAL_COMMENT_TWO");
+        String processFile = JdbcMapUtil.getString(entityRecord.valueMap,"FILE_ID_TWO");
+        //判断生成最终的意见和附件信息
+        Map<String,String> map2 = ProcessCommon.getEndComment(userName,processComment,processFile,comment,file);
+    }
+
 }
--- cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/ProcessCommon.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/ProcessCommon.java
@@ -5,8 +5,7 @@ import com.qygly.shared.util.JdbcMapUtil;
 import com.qygly.shared.util.SharedUtil;
 import org.springframework.util.CollectionUtils;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 流程通用扩展
@@ -18,7 +17,7 @@ public class ProcessCommon {
      * @param nodeId 节点id
      * @param userId 当前操作用户id
      * @param myJdbcTemplate 数据源
-     * @return
+     * @return 状态
      */
     public static Boolean getFirstCheck(String nodeId, String userId, MyJdbcTemplate myJdbcTemplate) {
         //判断是否是当轮拒绝回来的、撤销回来的（是否是第一个进入该节点审批的人）
@@ -32,5 +31,48 @@ public class ProcessCommon {
             }
         }
         return firstCheck;
+    }
+
+    /**
+     * 查询流程审批提交时的意见和附件信息
+     * @param procInstId 流程实例id
+     * @param userId 当前人员id
+     * @param myJdbcTemplate 数据源
+     * @return 意见信息
+     */
+    public static Map<String, String> getComment(String procInstId, String userId, MyJdbcTemplate myJdbcTemplate) {
+        Map<String,String> map = new HashMap<>();
+        String sql = "select tk.USER_COMMENT,tk.USER_ATTACHMENT from wf_node_instance ni " +
+                "join wf_task tk on ni.wf_process_instance_id=? and ni.is_current=1 and ni.id=tk.wf_node_instance_id and tk.status = 'ap' " +
+                "join ad_user u on tk.ad_user_id=u.id " +
+                "where u.id = ?";
+        List<Map<String, Object>> list = myJdbcTemplate.queryForList(sql, procInstId,userId);
+        String comment,file;
+        if (!CollectionUtils.isEmpty(list)) {
+            comment = SharedUtil.isEmptyString(JdbcMapUtil.getString(list.get(0),"user_comment"))  ? "同意" : JdbcMapUtil.getString(list.get(0),"user_comment");
+            file = JdbcMapUtil.getString(list.get(0),"USER_ATTACHMENT");
+            if (!SharedUtil.isEmptyString(file)){
+                map.put("file",file);
+            }
+            map.put("comment",comment);
+        }
+        return map;
+    }
+
+    /**
+     * 判断去重，避免同一个人重复提交相同文件
+     * @param userName 当前操作人名称
+     * @param processComment 流程中的意见信息
+     * @param processFile 流程中的文件信息
+     * @param comment 本次提交填写的意见信息
+     * @param file 本次提交填写的文件信息
+     * @return 提交信息
+     */
+    public static Map<String, String> getEndComment(String userName, String processComment, String processFile, String comment, String file) {
+        Map<String,String> map = new HashMap<>();
+        if (processComment.contains(userName)){
+            List<String> processCommentList = new ArrayList<>(Arrays.asList(processComment.split(";")));
+        }
+        return map;
     }
 }
