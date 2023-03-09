39e5d7bc05594d815da9649323a93f7ffe990d68
qf119ys <qf119ys@163.com>
2023-03-09 16:34:00 +0800

    feat:导入数据创建流程实例

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/archive/ArchiveGenerationJob.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/archive/ArchiveGenerationJob.java
@@ -18,7 +18,7 @@ public class ArchiveGenerationJob {
      */
     @Scheduled(fixedDelayString = "600000")
     public void execute() {
-        // archiveGenerationService.createProcInstForImportedData();
+        archiveGenerationService.createProcInstForImportedData();
         archiveGenerationService.execute();
     }
 }
