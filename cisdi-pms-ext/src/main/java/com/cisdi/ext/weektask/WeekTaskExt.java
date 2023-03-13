600f97b86151856c43bed991cdbb31520a89e2ba
yintao <yintao@ccscit.com>
2023-03-13 17:41:00 +0800

    feat:项目进展生成周任务

---
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/weektask/WeekTaskExt.java
@@ -0,0 +1,81 @@
+package com.cisdi.ext.weektask;
+
+import com.cisdi.ext.fund.FundReachApi;
+import com.cisdi.ext.util.JsonUtil;
+import com.cisdi.ext.util.StringUtil;
+import com.qygly.ext.jar.helper.ExtJarHelper;
+import com.qygly.ext.jar.helper.MyJdbcTemplate;
+import com.qygly.shared.util.JdbcMapUtil;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author 尹涛 * @version V1.0.0
+ * @projectName cisdi-pms-service
+ * @title WeekTaskExt
+ * @package com.cisdi.ext.weektask
+ * @description
+ * @date 2023/3/13
+ */
+public class WeekTaskExt {
+
+
+    /**
+     * 工作台本周工作列表
+     */
+    public void weekTaskList() {
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        Map<String, Object> map = ExtJarHelper.extApiParamMap.get();// 输入参数的map。
+        int pageSize = Integer.parseInt(String.valueOf(map.get("pageSize")));
+        int pageIndex = Integer.parseInt(String.valueOf(map.get("pageIndex")));
+        String userId = ExtJarHelper.loginInfo.get().userId;
+        userId = "0099799190825078670";
+        StringBuilder sb = new StringBuilder();
+        sb.append("select wt.*,gsv.`NAME` as task_status from week_task wt left join gr_set_value gsv on wt.WEEK_TASK_STATUS_ID = gsv.id  where AD_USER_ID = '").append(userId).append("' order by PUBLISH_START desc");
+
+        String totalSql = sb.toString();
+        int start = pageSize * (pageIndex - 1);
+        sb.append(" limit ").append(start).append(",").append(pageSize);
+        List<Map<String, Object>> list = myJdbcTemplate.queryForList(sb.toString());
+        List<WeekTask> weekTaskList = list.stream().map(p -> {
+            WeekTask weekTask = new WeekTask();
+            weekTask.id = JdbcMapUtil.getString(p, "ID");
+            weekTask.userId = JdbcMapUtil.getString(p, "AD_USER_ID");
+            weekTask.title = JdbcMapUtil.getString(p, "TITLE");
+            weekTask.content = JdbcMapUtil.getString(p, "CONTENT");
+            weekTask.publishStart = StringUtil.withOutT(JdbcMapUtil.getString(p, "PUBLISH_START"));
+            weekTask.taskStatus = JdbcMapUtil.getString(p, "task_status");
+            return weekTask;
+        }).collect(Collectors.toList());
+        if (CollectionUtils.isEmpty(weekTaskList)) {
+            ExtJarHelper.returnValue.set(Collections.emptyMap());
+        } else {
+            List<Map<String, Object>> totalList = myJdbcTemplate.queryForList(totalSql);
+            OutSide outSide = new OutSide();
+            outSide.total = totalList.size();
+            outSide.weekTaskList = weekTaskList;
+            Map outputMap = JsonUtil.fromJson(JsonUtil.toJson(outSide), Map.class);
+            ExtJarHelper.returnValue.set(outputMap);
+        }
+
+    }
+
+
+    public static class WeekTask {
+        public String id;
+        public String userId;
+        public String title;
+        public String content;
+        public String publishStart;
+        public String taskStatus;
+    }
+
+    public static class OutSide {
+        public List<WeekTask> weekTaskList;
+        public Integer total;
+    }
+}
--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/proPlan/ProPlanJob.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/proPlan/ProPlanJob.java
@@ -1,6 +1,8 @@
 package com.cisdi.pms.job.proPlan;
 
 import com.cisdi.pms.job.utils.ListUtils;
+import com.cisdi.pms.job.utils.Util;
+import com.cisdi.pms.job.utils.WeeklyUtils;
 import com.qygly.ext.rest.helper.feign.client.DataFeignClient;
 import com.qygly.ext.rest.helper.keeper.LoginInfoManager;
 import com.qygly.param.data.InvokeActParam;
@@ -16,8 +18,11 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.text.MessageFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -61,5 +66,32 @@ public class ProPlanJob {
                 }
             });
         });
+    }
+
+
+    /**
+     * 生成周任务
+     */
+//    @Scheduled(fixedDelayString = "${spring.scheduled.fixedDelayString}")
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void generateWeekTask() {
+        Date paramDate = WeeklyUtils.addDays(new Date(), 7);
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select pm.`NAME` as prjName,pppn.*,pi.AD_USER_ID as default_user from pm_pro_plan_node pppn " +
+                "left join pm_pro_plan ppp on ppp.id = pppn.PM_PRO_PLAN_ID " +
+                "left join pm_prj pm on pm.id = ppp.PM_PRJ_ID " +
+                "left join POST_INFO pi on pi.id = pppn.POST_INFO_ID " +
+                " where `level` = 3 and date_format(pppn.PLAN_START_DATE,'%Y-%m-%d') =? ;", "2025-01-16");
+
+        String msg = "{0}【{1}】计划将在{2}开始，请及时处理！";
+        for (Map<String, Object> objectMap : list) {
+            String id = Util.insertData(jdbcTemplate, "WEEK_TASK");
+            String userId = JdbcMapUtil.getString(objectMap, "CHIEF_USER_ID");
+            if (Objects.isNull(objectMap.get("CHIEF_USER_ID"))) {
+                userId = JdbcMapUtil.getString(objectMap, "default_user");
+            }
+            String title = MessageFormat.format(msg, objectMap.get("prjName"), objectMap.get("NAME"), objectMap.get("PLAN_START_DATE"));
+            jdbcTemplate.update("update WEEK_TASK set AD_USER_ID=?,TITLE=?,CONTENT=?,PUBLISH_START=?,WEEK_TASK_STATUS_ID=?,WEEK_TASK_TYPE_ID=?,RELATION_DATA_ID=? where id=?",
+                    userId, title, title, new Date(), "1634118574056542208", "1635080848313290752", objectMap.get("ID"), id);
+        }
     }
 }
