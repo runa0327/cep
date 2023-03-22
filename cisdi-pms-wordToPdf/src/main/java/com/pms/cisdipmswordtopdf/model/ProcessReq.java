19d4dab63908b269a88bd697ad267be2729d7af7
denglintao <denglintao@ccscit.com>
2023-03-22 10:00:57 +0800

    feat: 流程使用情况导出

---
+++ cisdi-pms-wordToPdf/src/main/java/com/pms/cisdipmswordtopdf/model/ProcessReq.java
@@ -0,0 +1,13 @@
+package com.pms.cisdipmswordtopdf.model;
+
+import lombok.Data;
+
+/**
+ * @author dlt
+ * @date 2023/3/21 周二
+ */
+@Data
+public class ProcessReq {
+    //开始日期
+    private String startDate;
+}
