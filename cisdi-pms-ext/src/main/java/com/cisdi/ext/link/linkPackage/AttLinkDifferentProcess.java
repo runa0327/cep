824b1ab68951459fd69b82ca4c5e3214b9521f2c
LiuXueF <1297157406@qq.com>
2023-05-05 09:19:18 +0800

    fix 合并冲突解决

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/link/linkPackage/AttLinkDifferentProcess.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/link/linkPackage/AttLinkDifferentProcess.java
@@ -171,9 +171,7 @@ public class AttLinkDifferentProcess {
         List<String> list = new ArrayList<>();
         list.add("PM_PRJ_SETTLE_ACCOUNTS"); //项目结算审批
         list.add("PM_EXTENSION_REQUEST_REQ"); //节点延期申请
-<<<<<<<<< Temporary merge branch 1
         list.add("PM_PRJ_RED_CHECK_REQ"); //项目红线核查
-=========
         list.add("PM_PRJ_REQ"); //立项申请审批
         list.add("PM_PRJ_INVEST1"); //可研报告审批
         list.add("PM_PRJ_INVEST2"); //初设概算审批
