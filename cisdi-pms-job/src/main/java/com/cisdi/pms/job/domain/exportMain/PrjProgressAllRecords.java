64adecf9b305eaa5fd7a06d929521b007aabe833
LiuXueF <1297157406@qq.com>
2023-06-09 16:16:34 +0800

    fix 形象进度工程周报导出表头及顺序调整

--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/domain/exportMain/PrjProgressAllRecords.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/domain/exportMain/PrjProgressAllRecords.java
@@ -11,23 +11,23 @@ public class PrjProgressAllRecords {
     @ExcelProperty("项目名称")
     public String projectName;
 
-    @ExcelProperty("填报时间")
-    public String writeDate;
-
     @ExcelProperty("项目负责人")
     public String manageUserName;
 
     @ExcelProperty("整体形象进度")
     public BigDecimal progress;
 
-    @ExcelProperty("累计形象进度（从开工至本周进展）")
-    public String progressDescribe;
-
     @ExcelProperty("本周项目进展")
     public String progressWeek;
 
+    @ExcelProperty("本周问题说明")
+    public String progressDescribe;
+
     @ExcelProperty("备注说明")
     public String progressRemark;
+
+    @ExcelProperty("填报时间")
+    public String writeDate;
 
     @ExcelProperty("是否符合开工条件")
     public String weatherStart;
