cce1715450f9ff23093468276db90fc6ec96bcd6
LiuXueF <1297157406@qq.com>
2023-06-09 15:21:14 +0800

    feat 财务对接接口-项目接口增加项目编号信息

--- cisdi-pms-externa-interface/src/main/java/com/cisdi/pms/serviceImpl/PmPrjServiceImpl.java
+++ cisdi-pms-externa-interface/src/main/java/com/cisdi/pms/serviceImpl/PmPrjServiceImpl.java
@@ -6,11 +6,11 @@ import com.cisdi.pms.util.StringUtilsNew;
 import com.qygly.shared.util.JdbcMapUtil;
 import com.qygly.shared.util.SharedUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -19,42 +19,46 @@ import java.util.Map;
 @Slf4j
 public class PmPrjServiceImpl implements PmPrjService {
 
-    @Autowired
+    @Resource
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<PmPrj> getBaseProject(PmPrj pmPrj) {
-        StringBuilder sb = new StringBuilder("select a.id,a.name,a.CRT_DT,a.CUSTOMER_UNIT,b.name as constructionUnitName,a.ESTIMATE_AMT,a.BUDGET_AMT,a.SETTLEMENT_AMT from pm_prj a " +
-                "left join PM_PARTY b on a.CUSTOMER_UNIT = b.id  where a.status = 'ap' and b.status = 'ap' ");
+        StringBuilder sb = new StringBuilder("select a.id,a.name,a.CRT_DT,a.CUSTOMER_UNIT,b.name as constructionUnitName,a.ESTIMATE_AMT,a.BUDGET_AMT,a.SETTLEMENT_AMT,a.PRJ_CODE as prjCode" +
+                " from pm_prj a left join PM_PARTY b on a.CUSTOMER_UNIT = b.id  " +
+                "where a.status = 'ap' and b.status = 'ap' ");
         if (!SharedUtil.isEmptyString(pmPrj.getProjectId())){
-            sb.append("and a.id = '"+pmPrj.getProjectId()+"' ");
+            sb.append("and a.id = '").append(pmPrj.getProjectId()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getProjectName())){
-            sb.append("and a.name like ('%"+pmPrj.getProjectName()+"%') ");
+            sb.append("and a.name like ('%").append(pmPrj.getProjectName()).append("%') ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getConstructionUnitId())){
-            sb.append("and a.CUSTOMER_UNIT = '"+pmPrj.getConstructionUnitId()+"' ");
+            sb.append("and a.CUSTOMER_UNIT = '").append(pmPrj.getConstructionUnitId()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getConstructionUnitName())){
-            sb.append("and b.name = '%"+pmPrj.getConstructionUnitName()+"%') ");
+            sb.append("and b.name = '%").append(pmPrj.getConstructionUnitName()).append("%') ");
+        }
+        if (!SharedUtil.isEmptyString(pmPrj.getPrjCode())){
+            sb.append("and a.PRJ_CODE = '%").append(pmPrj.getPrjCode()).append("%') ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getCreateTime())){
-            sb.append("and a.CRT_DT like ('%"+pmPrj.getCreateTime()+"%') ");
+            sb.append("and a.CRT_DT like ('%").append(pmPrj.getCreateTime()).append("%') ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getCreateTimeMin())){
-            sb.append("and a.CRT_DT >= '"+pmPrj.getCreateTimeMin()+"' ");
+            sb.append("and a.CRT_DT >= '").append(pmPrj.getCreateTimeMin()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getCreateTimeMax())){
-            sb.append("and a.CRT_DT <= '"+pmPrj.getCreateTimeMax()+"' ");
+            sb.append("and a.CRT_DT <= '").append(pmPrj.getCreateTimeMax()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getEstimateAmt())){
-            sb.append("and a.BUDGET_AMT = '"+pmPrj.getEstimateAmt()+"' ");
+            sb.append("and a.BUDGET_AMT = '").append(pmPrj.getEstimateAmt()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getSettlementAmt())){
-            sb.append("and a.SETTLEMENT_AMT = '"+pmPrj.getSettlementAmt()+"' ");
+            sb.append("and a.SETTLEMENT_AMT = '").append(pmPrj.getSettlementAmt()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getEstimateAmt())){
-            sb.append("and a.ESTIMATE_AMT = '"+pmPrj.getEstimateAmt()+"' ");
+            sb.append("and a.ESTIMATE_AMT = '").append(pmPrj.getEstimateAmt()).append("' ");
         }
 
         sb.append("order by a.CRT_DT asc ");
@@ -78,6 +82,7 @@ public class PmPrjServiceImpl implements PmPrjService {
                 pmPrj1.setBudgetAmt(JdbcMapUtil.getString(tmp,"BUDGET_AMT"));
                 pmPrj1.setSettlementAmt(JdbcMapUtil.getString(tmp,"SETTLEMENT_AMT"));
                 pmPrj1.setEstimateAmt(JdbcMapUtil.getString(tmp,"ESTIMATE_AMT"));
+                pmPrj1.setPrjCode(JdbcMapUtil.getString(tmp,"prjCode"));
                 pmPrjList.add(pmPrj1);
             }
         }
@@ -89,36 +94,36 @@ public class PmPrjServiceImpl implements PmPrjService {
         StringBuilder sb = new StringBuilder("select count(*) as num from pm_prj a " +
                 "left join PM_PARTY b on a.CUSTOMER_UNIT = b.id  where a.status = 'ap' and b.status = 'ap' ");
         if (!SharedUtil.isEmptyString(pmPrj.getProjectId())){
-            sb.append("and a.id = '"+pmPrj.getProjectId()+"' ");
+            sb.append("and a.id = '").append(pmPrj.getProjectId()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getProjectName())){
-            sb.append("and a.name like ('%"+pmPrj.getProjectName()+"%') ");
+            sb.append("and a.name like ('%").append(pmPrj.getProjectName()).append("%') ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getConstructionUnitId())){
-            sb.append("and a.CUSTOMER_UNIT = '"+pmPrj.getConstructionUnitId()+"' ");
+            sb.append("and a.CUSTOMER_UNIT = '").append(pmPrj.getConstructionUnitId()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getConstructionUnitName())){
-            sb.append("and b.name = '%"+pmPrj.getConstructionUnitName()+"%') ");
+            sb.append("and b.name = '%").append(pmPrj.getConstructionUnitName()).append("%') ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getCreateTime())){
-            sb.append("and a.CRT_DT like ('%"+pmPrj.getCreateTime()+"%') ");
+            sb.append("and a.CRT_DT like ('%").append(pmPrj.getCreateTime()).append("%') ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getCreateTimeMin())){
-            sb.append("and a.CRT_DT >= '"+pmPrj.getCreateTimeMin()+"' ");
+            sb.append("and a.CRT_DT >= '").append(pmPrj.getCreateTimeMin()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getCreateTimeMax())){
-            sb.append("and a.CRT_DT <= '"+pmPrj.getCreateTimeMax()+"' ");
+            sb.append("and a.CRT_DT <= '").append(pmPrj.getCreateTimeMax()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getEstimateAmt())){
-            sb.append("and a.BUDGET_AMT = '"+pmPrj.getEstimateAmt()+"' ");
+            sb.append("and a.BUDGET_AMT = '").append(pmPrj.getEstimateAmt()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getSettlementAmt())){
-            sb.append("and a.SETTLEMENT_AMT = '"+pmPrj.getSettlementAmt()+"' ");
+            sb.append("and a.SETTLEMENT_AMT = '").append(pmPrj.getSettlementAmt()).append("' ");
         }
         if (!SharedUtil.isEmptyString(pmPrj.getEstimateAmt())){
-            sb.append("and a.ESTIMATE_AMT = '"+pmPrj.getEstimateAmt()+"' ");
+            sb.append("and a.ESTIMATE_AMT = '").append(pmPrj.getEstimateAmt()).append("' ");
         }
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sb.toString());
-        return Integer.valueOf(JdbcMapUtil.getString(list.get(0),"num"));
+        return Integer.parseInt(JdbcMapUtil.getString(list.get(0),"num"));
     }
 }
