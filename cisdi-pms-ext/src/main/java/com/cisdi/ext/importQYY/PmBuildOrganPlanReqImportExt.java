da858859ca1f0ce2bdbca3a761fd6fb6046ca513
LiuXueF <1297157406@qq.com>
2023-07-12 15:17:54 +0800

    feat 施工组织设计及施工方案历史数据导入及文件归档

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/importQYY/PmBuildOrganPlanReqImportExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/importQYY/PmBuildOrganPlanReqImportExt.java
@@ -1,7 +1,7 @@
 package com.cisdi.ext.importQYY;
 
-import com.cisdi.ext.importQYY.model.PmSupervisePlanReqImport;
-import com.cisdi.ext.model.PmSupervisePlanReq;
+import com.cisdi.ext.importQYY.model.PmBuildOrganPlanReqImport;
+import com.cisdi.ext.model.PmBuildOrganPlanReq;
 import com.cisdi.ext.util.ImportUtil;
 import com.qygly.ext.jar.helper.ExtJarHelper;
 import com.qygly.ext.jar.helper.MyJdbcTemplate;
@@ -12,41 +12,41 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-public class PmSupervisePlanReqImportExt extends ImportUtil {
+public class PmBuildOrganPlanReqImportExt extends ImportUtil {
     @Override
     public Class getImportClass() {
-        return PmSupervisePlanReqImport.class;
+        return PmBuildOrganPlanReqImport.class;
     }
 
     @Override
     public List<Map<String, Object>> getOldData() {
         MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
-        List<Map<String, Object>> oldImports = myJdbcTemplate.queryForList("select id ,PM_PRJ_ID prjId from PM_SUPERVISE_PLAN_REQ where status = 'AP' and IS_IMPORT = 1 ");
+        List<Map<String, Object>> oldImports = myJdbcTemplate.queryForList("select id ,PM_PRJ_ID prjId from PM_BUILD_ORGAN_PLAN_REQ where status = 'AP' and IS_IMPORT = 1 ");
         return oldImports;
     }
 
     @Override
     public void doImport(Object dtl, List<Map<String, Object>> oldImportDataList) throws Exception {
-        PmSupervisePlanReqImport pmSupervisePlanReqImport = (PmSupervisePlanReqImport) dtl;
-        PmSupervisePlanReq pmSupervisePlanReq = PmSupervisePlanReq.newData();
+        PmBuildOrganPlanReqImport pmBuildOrganPlanReqImport = (PmBuildOrganPlanReqImport) dtl;
+        PmBuildOrganPlanReq pmBuildOrganPlanReq = PmBuildOrganPlanReq.newData();
         //比对是否导入过
         boolean needUpdate = false;
         if (!CollectionUtils.isEmpty(oldImportDataList)){
             Optional<Map<String, Object>> samePrjIdData = oldImportDataList.stream()
-                    .filter(singleData -> pmSupervisePlanReqImport.getPmPrjId().equals(singleData.get("prjId")))
+                    .filter(singleData -> pmBuildOrganPlanReqImport.getPmPrjId().equals(singleData.get("prjId")))
                     .findAny();
             if (samePrjIdData.isPresent()){
                 needUpdate = true;
-                pmSupervisePlanReq.setId(samePrjIdData.get().get("id").toString());
+                pmBuildOrganPlanReq.setId(samePrjIdData.get().get("id").toString());
             }
         }
-        BeanUtils.copyProperties(pmSupervisePlanReqImport,pmSupervisePlanReq,"id");
-        pmSupervisePlanReq.setIsImport(true);
+        BeanUtils.copyProperties(pmBuildOrganPlanReqImport,pmBuildOrganPlanReq,"id");
+        pmBuildOrganPlanReq.setIsImport(true);
 
         if (needUpdate) {
-            pmSupervisePlanReq.updateById();
+            pmBuildOrganPlanReq.updateById();
         }else{
-            pmSupervisePlanReq.insertById();
+            pmBuildOrganPlanReq.insertById();
         }
 
     }
--- cisdi-pms-job/src/main/java/com/cisdi/pms/job/archive/ArchiveGenerationService.java
+++ cisdi-pms-job/src/main/java/com/cisdi/pms/job/archive/ArchiveGenerationService.java
@@ -281,6 +281,10 @@ public class ArchiveGenerationService {
         map.put("PM_SUPERVISE_PLAN_REQ", "0099902212142023273");
         map2.put("PM_SUPERVISE_PLAN_REQ", "select T.* from PM_SUPERVISE_PLAN_REQ t where t.IS_IMPORT=1 and t.LK_WF_INST_ID is null and t.CRT_DT<date_add(now(),interval -5 minute)");
 
+        // 施工组织设计及施工方案
+        map.put("PM_BUILD_ORGAN_PLAN_REQ", "0099952822476361252");
+        map2.put("PM_BUILD_ORGAN_PLAN_REQ", "select T.* from PM_BUILD_ORGAN_PLAN_REQ t where t.IS_IMPORT=1 and t.LK_WF_INST_ID is null and t.CRT_DT<date_add(now(),interval -5 minute)");
+
         for (String entCode : map.keySet()) {
             String procId = map.get(entCode);
             Map<String, Object> ent = jdbcTemplate.queryForMap("select * from ad_ent where code=?", entCode);
