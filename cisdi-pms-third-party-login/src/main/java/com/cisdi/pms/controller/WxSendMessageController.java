712e4d2ea7aebfe44a6dbbc51968d997b5e5740e
yintao <yintao@ccscit.com>
2023-07-18 14:05:41 +0800

    fix:工作任务查看进度优化

--- cisdi-pms-third-party-login/src/main/java/com/cisdi/pms/controller/WxSendMessageController.java
+++ cisdi-pms-third-party-login/src/main/java/com/cisdi/pms/controller/WxSendMessageController.java
@@ -10,6 +10,7 @@ import com.cisdi.pms.service.WxSendMessageService;
 import com.sun.deploy.net.URLEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -41,7 +42,7 @@ public class WxSendMessageController {
      * @return
      */
     @PostMapping("sendMessage")
-    public JSONObject sendMessage(MessageModel messageModel) {
+    public JSONObject sendMessage(@RequestBody MessageModel messageModel) {
         SendMessageAttribute attribute = new SendMessageAttribute();
         attribute.setAnEnum(MsgTypeEnum.getMsgTypeEnum(messageModel.getType()));
         WxSendMessageService sendMessageService = factory.getService(attribute);
@@ -64,7 +65,7 @@ public class WxSendMessageController {
         cardInfo.setDescription("[工程项目信息协同系统][流程通知]你好“【紧急】采购需求审批-南繁科技城街道生态修复工程-设计施工总承包（EPC）-黄锦涛-2023-07-04 15:39:50”已到您处，请登录系统查看");
         StringBuilder sb = new StringBuilder("https://cpms.yazhou-bay.com/h5/unifiedLogin?env=ZWWeiXin");
         sb.append("&path=").append(messageModel.getPathSuffix());
-        String ada = URLEncoder.encode(sb.toString(),"utf-8");
+        String ada = URLEncoder.encode(sb.toString(), "utf-8");
         cardInfo.setUrl(MessageFormat.format(url, ada));
 //        cardInfo.setUrl("https://open.weixin.qq.com/connect/oauth2/authorize?appid=ww808726c44a3ff6dc&redirect_uri=https%3A%2F%2Fcpms.yazhou-bay.com%2Fh5%2FunifiedLogin%3Fenv%3DZWWeiXin&response_type=code&scope=snsapi_userinfo&agentid=1000005&state=STATE#wechat_redirect");
         messageModel.setObject(cardInfo);
