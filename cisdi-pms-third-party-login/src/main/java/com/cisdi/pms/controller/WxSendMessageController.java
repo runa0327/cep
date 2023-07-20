6ed86e36d985fee70f33338f1c9cc16c6b57c647
LiuXueF <1297157406@qq.com>
2023-07-20 10:55:14 +0800

    fix

--- cisdi-pms-third-party-login/src/main/java/com/cisdi/pms/controller/WxSendMessageController.java
+++ cisdi-pms-third-party-login/src/main/java/com/cisdi/pms/controller/WxSendMessageController.java
@@ -7,7 +7,6 @@ import com.cisdi.pms.model.TextCardInfo;
 import com.cisdi.pms.service.SendMessageAttribute;
 import com.cisdi.pms.service.SendMessageFactory;
 import com.cisdi.pms.service.WxSendMessageService;
-import com.sun.deploy.net.URLEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.MessageFormat;
 import java.util.Arrays;
 
