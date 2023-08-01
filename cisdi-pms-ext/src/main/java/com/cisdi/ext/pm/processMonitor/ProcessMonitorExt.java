e2ee7ca7a38e4eed63cd0e4e4626fef00f0a84ba
LiuXueF <1297157406@qq.com>
2023-08-01 15:59:13 +0800

    fix

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/processMonitor/ProcessMonitorExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/pm/processMonitor/ProcessMonitorExt.java
@@ -17,12 +17,11 @@ import java.util.Map;
  */
 public class ProcessMonitorExt {
 
-    MyJdbcTemplate jdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
-
     /**
      * 查询截止当前所有流程总数(起始日期 2022-11-22)
      */
     public void getAllProcessInstance(){
+        MyJdbcTemplate jdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
         String sql = "select ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681'";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
         if (!CollectionUtils.isEmpty(list)){
@@ -37,6 +36,7 @@ public class ProcessMonitorExt {
      * 查询截止当前所有流程大类总数(起始日期 2022-11-22)
      */
     public void getAllProcessGroupByType(){
+        MyJdbcTemplate jdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
         String sql = "select c.name,ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id LEFT JOIN wf_cate c on b.wf_cate_id = c.id where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' group by c.id ORDER BY num desc";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
         if (!CollectionUtils.isEmpty(list)){
@@ -58,15 +58,16 @@ public class ProcessMonitorExt {
      * 区间范围内发起、办理、办结 TOP5 数
      */
     public void getTop5ProcessCate(){
+        MyJdbcTemplate jdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
         Map<String, Object> map = ExtJarHelper.extApiParamMap.get();// 输入参数的map。
         String json = JsonUtil.toJson(map);
         ProcessMonitor input = JsonUtil.fromJson(json, ProcessMonitor.class);
         String start = input.getStartTime();
         String end = input.getEndTime();
 
-        List<ProcessMonitor> startList = getStartTop(start,end);
-        List<ProcessMonitor> endList = getEndTop(start,end);
-        List<ProcessMonitor> checkList = getCheckTop(start,end);
+        List<ProcessMonitor> startList = getStartTop(start,end,jdbcTemplate);
+        List<ProcessMonitor> endList = getEndTop(start,end,jdbcTemplate);
+        List<ProcessMonitor> checkList = getCheckTop(start,end,jdbcTemplate);
 
         Map<String,List<ProcessMonitor>> resMap = new HashMap<>();
         resMap.put("start",startList);
@@ -81,9 +82,10 @@ public class ProcessMonitorExt {
      * 获取时间内审批的TOP5流程
      * @param start 开始时间
      * @param end 结束时间
+     * @param jdbcTemplate 数据源
      * @return 查询结果
      */
-    private List<ProcessMonitor> getCheckTop(String start, String end) {
+    private List<ProcessMonitor> getCheckTop(String start, String end, MyJdbcTemplate jdbcTemplate) {
         List<ProcessMonitor> res = new ArrayList<>();
         String sql = "select c.name,ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id LEFT JOIN wf_cate c on b.wf_cate_id = c.id LEFT JOIN wf_node_instance d on a.id = d.wf_process_instance_id left join wf_node e on d.wf_node_id = e.id where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' and d.end_dateTime >= ? and d.end_dateTime <= ? and e.node_type = 'user_task' group by c.id ORDER BY num desc limit 5";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,start,end);
@@ -97,9 +99,10 @@ public class ProcessMonitorExt {
      * 获取时间内办结的TOP5流程
      * @param start 开始时间
      * @param end 结束时间
+     * @param jdbcTemplate 数据源
      * @return 查询结果
      */
-    private List<ProcessMonitor> getEndTop(String start, String end) {
+    private List<ProcessMonitor> getEndTop(String start, String end, MyJdbcTemplate jdbcTemplate) {
         List<ProcessMonitor> res = new ArrayList<>();
         String sql = "select c.name,ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id LEFT JOIN wf_cate c on b.wf_cate_id = c.id where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' and a.end_dateTime >= ? and a.end_dateTime <= ? group by c.id ORDER BY num desc limit 5";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,start,end);
@@ -113,9 +116,10 @@ public class ProcessMonitorExt {
      * 获取时间内发起的TOP5流程
      * @param start 开始时间
      * @param end 结束时间
+     * @param jdbcTemplate 数据源
      * @return 查询结果
      */
-    private List<ProcessMonitor> getStartTop(String start, String end) {
+    private List<ProcessMonitor> getStartTop(String start, String end, MyJdbcTemplate jdbcTemplate) {
         List<ProcessMonitor> res = new ArrayList<>();
         String sql = "select c.name,ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id LEFT JOIN wf_cate c on b.wf_cate_id = c.id where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' and a.crt_dt >= ? and a.crt_dt <= ? group by c.id ORDER BY num desc limit 5";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql,start,end);
@@ -143,6 +147,7 @@ public class ProcessMonitorExt {
      * 流程处理半年趋势图
      */
     public void processHalfYear(){
+        MyJdbcTemplate jdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
         String sql = "select a.month,a.num as chechNum,b.num as startNum,c.num as endNum from (select DATE_FORMAT(d.end_dateTime,'%Y-%m') as month,ifnull(count(distinct a.id),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id  LEFT JOIN wf_node_instance d on a.id = d.wf_process_instance_id left join wf_node e on d.wf_node_id = e.id where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' and d.end_dateTime >= DATE_SUB(DATE_FORMAT(NOW(),'%Y-%m-01'),INTERVAL 6 MONTH) and e.node_type = 'user_task' group by month ORDER BY month desc) a LEFT JOIN (select DATE_FORMAT(a.crt_dt,'%Y-%m') as month,ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id  where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' and a.crt_dt >= DATE_SUB(DATE_FORMAT(NOW(),'%Y-%m-01'),INTERVAL 6 MONTH)  group by month ORDER BY month desc ) b on a.month = b.month LEFT JOIN (select DATE_FORMAT(a.crt_dt,'%Y-%m') as month,ifnull(count(*),0) as num from wf_process_instance a left join wf_process b on a.wf_process_id = b.id  where a.status = 'ap' and b.status in ('ap','aping') and a.crt_dt >= '2022-11-22 00:00:00' and START_USER_ID != '0099250247095871681' and a.end_dateTime >= DATE_SUB(DATE_FORMAT(NOW(),'%Y-%m-01'),INTERVAL 6 MONTH)  group by month ORDER BY month desc ) c on a.month = c.month order by a.month asc";
         List<Map<String,Object>> list = jdbcTemplate.queryForList(sql);
         if (!CollectionUtils.isEmpty(list)){
