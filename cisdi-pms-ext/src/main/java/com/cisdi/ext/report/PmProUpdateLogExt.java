5b53d00c58a84725eaf98babf9d4bcc7c930bb7c
LiuXueF <1297157406@qq.com>
2023-08-08 19:54:09 +0800

    feat 数据台账修改增加日志记录及校验核对

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/report/PmProUpdateLogExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/report/PmProUpdateLogExt.java
@@ -19,8 +19,9 @@ public class PmProUpdateLogExt {
      * @param userId 操作人id
      * @param now 当前时间
      * @param msg 操作说明
+     * @param operationType 操作类型
      */
-    public static void createData(String entCode, String sourceId, String prjId, String proInstanceId, String userId, String now, String msg) {
+    public static void createData(String entCode, String sourceId, String prjId, String proInstanceId, String userId, String now, String msg, String operationType) {
         String id = Crud.from(PmProUpdateLog.ENT_CODE).insertData();
         Crud.from(PmProUpdateLog.ENT_CODE).where().eq(PmProUpdateLog.Cols.ID,id).update()
                 .set(PmProUpdateLog.Cols.WF_PROCESS_INSTANCE_ID,proInstanceId)
@@ -28,7 +29,7 @@ public class PmProUpdateLogExt {
                 .set(PmProUpdateLog.Cols.AD_ENT_ID,entCode)
                 .set(PmProUpdateLog.Cols.ENTITY_RECORD_ID,sourceId)
                 .set(PmProUpdateLog.Cols.AD_USER_ID,userId)
-                .set(PmProUpdateLog.Cols.OPERATION_TYPE_ID,"1688359200281030656")
+                .set(PmProUpdateLog.Cols.OPERATION_TYPE_ID,operationType)
                 .set(PmProUpdateLog.Cols.STATUS,"AP")
                 .set(PmProUpdateLog.Cols.SUBMIT_TIME,now)
                 .set(PmProUpdateLog.Cols.TEXT_REMARK_ONE,msg)
--- cisdi-pms-ext/src/main/java/com/cisdi/ext/report/ReportCommon.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/report/ReportCommon.java
@@ -2,14 +2,17 @@ package com.cisdi.ext.report;
 
 import com.cisdi.ext.base.PmPrjExt;
 import com.cisdi.ext.model.AdEnt;
+import com.cisdi.ext.pm.processCommon.ProcessCommon;
 import com.cisdi.ext.util.DateTimeUtil;
 import com.qygly.ext.jar.helper.ExtJarHelper;
 import com.qygly.ext.jar.helper.MyJdbcTemplate;
 import com.qygly.ext.jar.helper.sql.Crud;
 import com.qygly.ext.jar.helper.sql.Where;
+import com.qygly.shared.BaseException;
 import com.qygly.shared.interaction.EntityRecord;
 import com.qygly.shared.util.JdbcMapUtil;
 import com.qygly.shared.util.SharedUtil;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -30,6 +33,10 @@ public class ReportCommon {
         COMMON_MAP.put("IS_PRESET","IS_PRESET");
         COMMON_MAP.put("LAST_MODI_DT","LAST_MODI_DT");
         COMMON_MAP.put("LAST_MODI_USER_ID","LAST_MODI_USER_ID");
+        COMMON_MAP.put("IS_CHECK_OVER","IS_CHECK_OVER");
+        COMMON_MAP.put("IS_HISTORY_DATA","IS_HISTORY_DATA");
+        COMMON_MAP.put("IS_OMPORT","IS_OMPORT");
+        COMMON_MAP.put("CODE","CODE");
     }
 
     /**
@@ -43,7 +50,7 @@ public class ReportCommon {
         String userId = ExtJarHelper.loginInfo.get().userId;
         String now = DateTimeUtil.dttmToString(new Date());
         String userName = ExtJarHelper.loginInfo.get().userName;
-        StringBuilder sb = new StringBuilder("用户 ").append(userName).append(" 在 ").append(now);
+        StringBuilder sb = new StringBuilder("用户:").append(userName).append(" 在 ").append(now);
         for (EntityRecord tmp : list) {
             Map<String,Object> map = tmp.valueMap;
             String proInstanceId = JdbcMapUtil.getString(map,"LK_WF_INST_ID");
@@ -54,10 +61,71 @@ public class ReportCommon {
                 String[] prjArr = projectId.split(",");
                 for (String prj : prjArr) {
                     sb.append(" 对项目id为：").append(prj).append(" ,流程实例id为：").append(proInstanceId).append(",记录id为：").append(id).append(" 的数据点击了确认按钮！");
-                    PmProUpdateLogExt.createData(codeId,id,prj,proInstanceId,userId,now,sb.toString());
+                    PmProUpdateLogExt.createData(codeId,id,prj,proInstanceId,userId,now,sb.toString(),"1688359200281030656");
                 }
                 Crud.from(entCode).where().eq("ID",id).update().set("IS_CHECK_OVER",1).exec();
             }
         }
+    }
+
+    /**
+     * 台账数据修改
+     */
+    public void updateProData(){
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        String entCode = ExtJarHelper.sevInfo.get().entityInfo.code;
+        String codeId = AdEnt.selectByWhere(new Where().eq(AdEnt.Cols.CODE,entCode)).get(0).getId();
+        List<EntityRecord> list = ExtJarHelper.entityRecordList.get();
+        String userId = ExtJarHelper.loginInfo.get().userId;
+        String now = DateTimeUtil.dttmToString(new Date());
+        String userName = ExtJarHelper.loginInfo.get().userName;
+        StringBuilder sb = new StringBuilder("用户:").append(userName).append(" 在 ").append(now);
+        for (EntityRecord tmp : list) {
+            Map<String,Object> map = tmp.valueMap;
+            String isCheck = JdbcMapUtil.getString(map,"IS_CHECK_OVER");
+            if ("1".equals(isCheck)){
+                throw new BaseException("抱歉，该条数据已核对完毕，不允许被修改！");
+            } else {
+                String prj = PmPrjExt.getProjectIdByProcess(map);
+                String proInstanceId = JdbcMapUtil.getString(map,"LK_WF_INST_ID");
+                String id = JdbcMapUtil.getString(map,"ID");
+                map.entrySet().removeIf(p->COMMON_MAP.containsKey(p.getKey()));
+                Map<String,Object> oldMap = myJdbcTemplate.queryForMap("select * from " + entCode + " where id = ?",id);
+                StringBuilder nsb = getUpdateChangeMsg(map,oldMap);
+                if (StringUtils.hasText(nsb)){
+                    sb.append(" 时间修改了数据，修改内容有：").append(nsb);
+                } else {
+                    sb.append(" 时间点击了修改按钮，但是没有修改数据");
+                }
+                PmProUpdateLogExt.createData(codeId,id,prj,proInstanceId,userId,now,sb.toString(),"1688359123244249088");
+            }
+        }
+    }
+
+    /**
+     * 台账数据修改，比较判断新老数据变化的地址
+     * @param newMap 修改后map
+     * @param oldMap 修改前map
+     * @return 变化记录
+     */
+    private StringBuilder getUpdateChangeMsg(Map<String, Object> newMap, Map<String, Object> oldMap) {
+        StringBuilder sb = new StringBuilder();
+        for (String key : newMap.keySet()){
+            if (oldMap.containsKey(key)){
+                String newValue = JdbcMapUtil.getString(newMap,key);
+                String oldValue = JdbcMapUtil.getString(oldMap,key);
+                if (!StringUtils.hasText(newValue) && !StringUtils.hasText(oldValue)){
+                    continue;
+                } else if (StringUtils.hasText(newValue) && StringUtils.hasText(oldValue)){
+                    if (!newValue.equals(oldValue)){
+                        sb.append("字段：").append(key).append(",修改前值为：").append(oldValue).append(";修改后值为：").append(newValue).append(";");
+                    }
+                } else {
+                    sb.append("字段：").append(key).append(",修改前值为：").append(oldValue).append(";修改后值为：").append(newValue).append(";");
+                }
+
+            }
+        }
+        return sb;
     }
 }
