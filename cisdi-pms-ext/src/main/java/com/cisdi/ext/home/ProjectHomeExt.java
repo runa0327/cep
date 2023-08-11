6a7249ffd84c95e808669921de710b4d6c49abed
yintao <yintao@ccscit.com>
2023-08-11 13:54:06 +0800

    feat:项目首页功能开发

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/home/ProjectHomeExt.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/home/ProjectHomeExt.java
@@ -1,6 +1,5 @@
 package com.cisdi.ext.home;
 
-import com.cisdi.ext.util.BigDecimalUtil;
 import com.cisdi.ext.util.JsonUtil;
 import com.cisdi.ext.util.StringUtil;
 import com.cisdi.ext.util.WeeklyUtils;
@@ -12,7 +11,6 @@ import org.apache.logging.log4j.util.Strings;
 import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -82,53 +80,27 @@ public class ProjectHomeExt {
         Map<String, Object> map = ExtJarHelper.extApiParamMap.get();// 输入参数的map。
         String projectId = String.valueOf(map.get("projectId"));
         BigDecimal totalAmt = BigDecimal.ZERO;
-        BigDecimal planAmt = BigDecimal.ZERO;
-        BigDecimal completeAmt = BigDecimal.ZERO;
-        List<Map<String, Object>> dataList = myJdbcTemplate.queryForList("select ifnull(pie.PRJ_TOTAL_INVEST,0) as PRJ_TOTAL_INVEST from pm_invest_est pie \n" +
+        BigDecimal jaAmt = BigDecimal.ZERO;
+        BigDecimal jaCompleteAmt = BigDecimal.ZERO;
+        List<Map<String, Object>> dataList = myJdbcTemplate.queryForList("select ifnull(pie.PRJ_TOTAL_INVEST,0) as PRJ_TOTAL_INVEST,pie.id as id from pm_invest_est pie \n" +
                 "left join gr_set_value gsv on gsv.id = pie.INVEST_EST_TYPE_ID \n" +
                 "where pie.PM_PRJ_ID=? and PRJ_TOTAL_INVEST<>0 \n" +
                 "order by gsv.code desc limit 0,1", projectId);
         if (!CollectionUtils.isEmpty(dataList)) {
-            totalAmt = new BigDecimal(String.valueOf(dataList.get(0).get("PRJ_TOTAL_INVEST")));
-        }
-        totalAmt = BigDecimalUtil.divide(totalAmt, new BigDecimal(10000));
-        //今年计划投资-取投资计划汇总
-        int currentYear = LocalDate.now().getYear();
-        List<Map<String, Object>> list = myJdbcTemplate.queryForList("select ifnull(AMT,0) as AMT from PM_INVESTMENT_YEAR_PLAN where PM_PRJ_ID =? and `year`=?", projectId, currentYear);
-        if (!CollectionUtils.isEmpty(list)) {
-            planAmt = new BigDecimal(String.valueOf(list.get(0).get("AMT")));
-        }
-        planAmt = BigDecimalUtil.divide(planAmt, new BigDecimal(10000));
-        //今年完成投资-取纳统汇总
-        String sql = " select id,\n" +
-                "        PM_PRJ_ID,\n" +
-                "        year,\n" +
-                "        month,\n" +
-                "        ifnull(architectural_engineering_fee,0) as architectural_engineering_fee,\n" +
-                "        ifnull(installation_engineering_fee,0) as installation_engineering_fee,\n" +
-                "        ifnull(equipment_purchase_fee,0) as equipment_purchase_fee,\n" +
-                "        ifnull(other_fee,0) as other_fee,\n" +
-                "        ifnull(this_month_investment,0) as this_month_investment ,\n" +
-                "        ifnull(residential,0) as residential,\n" +
-                "        ifnull(purchase_old_equipment,0) as purchase_old_equipment,\n" +
-                "        ifnull(purchase_old_building,0) as purchase_old_building,\n" +
-                "        ifnull(construction_land_charge,0) as construction_land_charge,\n" +
-                "        ifnull(this_year_investment,0) as this_year_investment\n" +
-                "        from PM_STATISTICS_FEE\n" +
-                "        where PM_PRJ_ID=? and year=? " +
-                "        order by month";
-        List<Map<String, Object>> feeList = myJdbcTemplate.queryForList(sql, projectId, currentYear);
-        if (!CollectionUtils.isEmpty(feeList)) {
-            completeAmt = BigDecimalUtil.stringToBigDecimal(String.valueOf(feeList.get(0).get("architectural_engineering_fee")))
-                    .add(BigDecimalUtil.stringToBigDecimal(String.valueOf(feeList.get(0).get("installation_engineering_fee"))))
-                    .add(BigDecimalUtil.stringToBigDecimal(String.valueOf(feeList.get(0).get("equipment_purchase_fee"))))
-                    .add(BigDecimalUtil.stringToBigDecimal(String.valueOf(feeList.get(0).get("other_fee"))));
+            Map<String, Object> mapData = dataList.get(0);
+            totalAmt = new BigDecimal(String.valueOf(mapData.get("PRJ_TOTAL_INVEST")));
+            List<Map<String, Object>> list = myJdbcTemplate.queryForList("select ifnull(amt,0) as amt from pm_invest_est_dtl dt left join pm_exp_type et on dt.PM_EXP_TYPE_ID = et.id where et.`CODE`='CONSTRUCT_AMT' and PM_INVEST_EST_ID=? ", mapData.get("id"));
+            if(!CollectionUtils.isEmpty(list)){
+                Map<String, Object> jaMapData = dataList.get(0);
+                jaAmt =  new BigDecimal(String.valueOf(jaMapData.get("amt")));
+            }
+
         }
 
         OutSide outSide = new OutSide();
-        outSide.totalAmt = totalAmt.multiply(new BigDecimal(10000));
-        outSide.planAmt = planAmt.multiply(new BigDecimal(10000));
-        outSide.completeAmt = completeAmt.multiply(new BigDecimal(10000));
+        outSide.totalAmt = totalAmt;
+        outSide.jaAmt = jaAmt;
+        outSide.jaCompleteAmt = jaCompleteAmt;
         Map outputMap = JsonUtil.fromJson(JsonUtil.toJson(outSide), Map.class);
         ExtJarHelper.returnValue.set(outputMap);
     }
@@ -280,9 +252,9 @@ public class ProjectHomeExt {
     public static class OutSide {
         public BigDecimal totalAmt;
 
-        public BigDecimal planAmt;
+        public BigDecimal jaAmt;
 
-        public BigDecimal completeAmt;
+        public BigDecimal jaCompleteAmt;
 
         public List<FileObj> fileObjList;
 
