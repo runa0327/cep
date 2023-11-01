0ee177bd25d4aaafa43f5530d019a4e878606fdf
LiuXueF <1297157406@qq.com>
2023-11-01 19:05:51 +0800

    fix 花名册取数逻辑fix

--- cisdi-pms-ext/src/main/java/com/cisdi/ext/util/WfPmInvestUtil.java
+++ cisdi-pms-ext/src/main/java/com/cisdi/ext/util/WfPmInvestUtil.java
@@ -13,7 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
-import java.math.BigDecimal;
 import java.util.*;
 import java.util.stream.Collectors;
 
