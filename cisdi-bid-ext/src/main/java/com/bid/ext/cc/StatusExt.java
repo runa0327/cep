38671365b1c685eace28128f847d287048117609
xiamulan <758902836@qq.com>
2024-02-23 17:19:35 +0800

    巡检流程状态变更

---
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/StatusExt.java
@@ -0,0 +1,50 @@
+package com.bid.ext.cc;
+
+import com.qygly.ext.jar.helper.ExtJarHelper;
+import com.qygly.ext.jar.helper.MyJdbcTemplate;
+import com.qygly.shared.ad.entity.EntityInfo;
+import com.qygly.shared.ad.sev.SevInfo;
+import com.qygly.shared.interaction.EntityRecord;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+public class StatusExt {
+
+    public void changeStatusToZG() {
+        //整改
+        String STATE_ID = "1750797105878466560";
+        changeStatus(STATE_ID);
+    }
+
+    public void changeStatusToFH() {
+        //复核
+        String STATE_ID = "1750797109250686976";
+        changeStatus(STATE_ID);
+    }
+
+    public void changeStatusToHG() {
+        //合格
+        String STATE_ID = "1750797111708549120";
+        changeStatus(STATE_ID);
+    }
+    private void changeStatus(String STATE_ID){
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        SevInfo sevInfo = ExtJarHelper.sevInfo.get();
+        EntityInfo entityInfo = sevInfo.entityInfo;
+        String entityCode = entityInfo.code;
+        List<EntityRecord> entityRecordList = ExtJarHelper.entityRecordList.get();
+
+        if (entityRecordList != null) {
+            for (EntityRecord entityRecord : entityRecordList) {
+                String csCommId = entityRecord.csCommId;
+                Map<String, Object> valueMap = entityRecord.valueMap;
+                int update = myJdbcTemplate.update("update " + entityCode + " t set t.CC_QS_CURRENT_STATE_ID = ? where t.id=?",STATE_ID , csCommId);
+                log.info("已更新：{}", update);
+            }
+        }
+    }
+
+}
