42c2fa17e52da309f8a4a6075442fe13bdcd28ca
xiamulan <758902836@qq.com>
2024-03-15 13:52:55 +0800

    修改自动添加名称

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/VerifyFileExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/VerifyFileExt.java
@@ -8,46 +8,41 @@ import com.qygly.shared.ad.sev.SevInfo;
 import com.qygly.shared.interaction.EntityRecord;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 public class VerifyFileExt {
-    EntityRecord entityRecord = ExtJarHelper.entityRecordList.get().get(0);
-    String csCommId = entityRecord.csCommId;
-    Map<String, Object> valueMap = entityRecord.valueMap;
-    SevInfo sevInfo = ExtJarHelper.sevInfo.get();
-    EntityInfo entityInfo = sevInfo.entityInfo;
-    String entityCode = entityInfo.code;
-    MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
-    private Map<String, String> getFileStats() {
-        String fileId = String.valueOf(valueMap.get("CC_ATTACHMENT"));
-        String extSql = "select f.ext,f.dsp_name from fl_file f where id = ?";
-        Map<String, Object> queryResult = myJdbcTemplate.queryForMap(extSql, fileId);
-        String extName = queryResult.get("ext").toString();
-        String fileName = queryResult.get("dsp_name").toString();
-
-        Map<String, String> result = new HashMap<>();
-        result.put("extName", extName);
-        result.put("fileName", fileName);
-        return result;
-    }
 
     public void insertFileName(){
-        Map<String, String> fileStats = getFileStats();
-        String fileName = fileStats.get("fileName");
-        int update = myJdbcTemplate.update("update " + entityCode + " t set t.NAME = ? where t.id=?", fileName, csCommId);
-        log.info("Updated: {}", update);
+        EntityRecord entityRecord = ExtJarHelper.entityRecordList.get().get(0);
+        String csCommId = entityRecord.csCommId;
+        Map<String, Object> valueMap = entityRecord.valueMap;
+        SevInfo sevInfo = ExtJarHelper.sevInfo.get();
+        EntityInfo entityInfo = sevInfo.entityInfo;
+        String entityCode = entityInfo.code;
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        String fileId = String.valueOf(valueMap.get("CC_ATTACHMENT"));
+        String extSql = "select f.ext,f.dsp_name from fl_file f where id = ?";
+        String fileName = myJdbcTemplate.queryForMap(extSql,fileId).get("dsp_name").toString();
+        int update = myJdbcTemplate.update("update " + entityCode + " t set t.NAME = ? where t.id=?",fileName,csCommId);
     }
 
-    public void verify() {
-        Map<String, String> fileStats = getFileStats();
-        String extName = fileStats.get("extName");
-        String fileName = fileStats.get("fileName");
-
+    public void verify(){
+        EntityRecord entityRecord = ExtJarHelper.entityRecordList.get().get(0);
+        String csCommId = entityRecord.csCommId;
+        Map<String, Object> valueMap = entityRecord.valueMap;
+        SevInfo sevInfo = ExtJarHelper.sevInfo.get();
+        EntityInfo entityInfo = sevInfo.entityInfo;
+        String entityCode = entityInfo.code;
+        String fileId = String.valueOf(valueMap.get("CC_ATTACHMENT"));
         Object filetypeIdObj = valueMap.get("CC_DOC_FILE_TYPE_ID");
         String filetypeId = filetypeIdObj != null ? filetypeIdObj.toString() : null;
+
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.myJdbcTemplate.get();
+        String extSql = "select f.ext,f.dsp_name from fl_file f where id = ?";
+        String extName = myJdbcTemplate.queryForMap(extSql,fileId).get("ext").toString();
+        String fileName = myJdbcTemplate.queryForMap(extSql,fileId).get("dsp_name").toString();
 
         if (filetypeId == null || filetypeId.isEmpty()) {
             String allFileTypeSql = "select id, FILE_EXTENSION from CC_DOC_FILE_TYPE";
@@ -62,33 +57,35 @@ public class VerifyFileExt {
                     }
                 }
                 if (matchedTypeId != null) {
-                    break;
+                    break; // 找到匹配项后退出循环
                 }
             }
 
             if (matchedTypeId == null) {
                 // 如果没有找到匹配项
-                throw new BaseException("无法识别当前上传文件的类型（." + extName + "），请联系管理员添加所需的文件类型。");
+                throw new BaseException("不能识别当前上传文件类型(." + extName + ")，请联系管理员添加所需文件类型");
             } else {
-                //更新
-                int update = myJdbcTemplate.update("update " + entityCode + " t set t.CC_DOC_FILE_TYPE_ID = ?,t.NAME = ? where t.id=?", matchedTypeId, fileName, csCommId);
-                log.info("Updated: {}", update);
+                int update = myJdbcTemplate.update("update " + entityCode + " t set t.CC_DOC_FILE_TYPE_ID = ?,t.NAME = ? where t.id=?",matchedTypeId,fileName,csCommId);
+                log.info("已更新：{}", update);
             }
-        } else {
+        }
+
+        else {
+            // filetypeId不为空的处理逻辑
             String extTypeSql = "select ft.FILE_EXTENSION from CC_DOC_FILE_TYPE ft where id = ?";
-            String[] fileExtension = myJdbcTemplate.queryForMap(extTypeSql, filetypeId).get("FILE_EXTENSION").toString().split(";");
+            String[] fileExtension = myJdbcTemplate.queryForMap(extTypeSql, filetypeId).get("file_extension").toString().split(";");
             boolean isMatch = false;
             for (String extension : fileExtension) {
                 if (extName.equals(extension.trim())) {
                     isMatch = true;
-                    break;
+                    break; // 找到匹配项后退出循环
                 }
             }
+
             if (!isMatch) {
                 // 如果没有找到匹配项
-                throw new BaseException("资料文件类型与上传文件的类型（." + extName + "）不匹配，请选择正确的文件类型或者不选择文件类型。");
+                throw new BaseException("资料文件类型与上传文件类型(." + extName + ")不匹配，请选择正确的文件类型或不选择文件类型");
             }
         }
     }
-
 }
