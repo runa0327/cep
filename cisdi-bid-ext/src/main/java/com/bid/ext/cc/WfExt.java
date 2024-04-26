e481d1ecb72a0efd4e7f445faed4afaa4fe169bc
qiufei123456 <8080707+qiufei123456@user.noreply.gitee.com>
2024-04-26 10:54:21 +0800

    审批中更改状态为：DR、APING、DN、AP、VDING、VD

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/WfExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/WfExt.java
@@ -2,10 +2,13 @@ package com.bid.ext.cc;
 
 import com.bid.ext.model.CcPrjToProcInst;
 import com.qygly.ext.jar.helper.ExtJarHelper;
+import com.qygly.ext.jar.helper.MyJdbcTemplate;
 import com.qygly.ext.jar.helper.sql.Crud;
 import com.qygly.ext.jar.helper.sql.Where;
 import com.qygly.shared.SharedConstants;
+import com.qygly.shared.ad.entity.StatusE;
 import com.qygly.shared.interaction.EntityRecord;
+import com.qygly.shared.util.EntityRecordUtil;
 import com.qygly.shared.util.SharedUtil;
 
 public class WfExt {
@@ -41,5 +44,38 @@ public class WfExt {
         }
 
 
+    }
+
+    public void setStatusToDr() {
+        setStatus(StatusE.DR);
+    }
+
+    public void setStatusToAping() {
+        setStatus(StatusE.APING);
+    }
+
+    public void setStatusToDn() {
+        setStatus(StatusE.DN);
+    }
+
+    public void setStatusToAp() {
+        setStatus(StatusE.AP);
+    }
+
+    public void setStatusToVding() {
+        setStatus(StatusE.VDING);
+    }
+
+    public void setStatusToVd() {
+        setStatus(StatusE.VD);
+    }
+
+    private void setStatus(StatusE status) {
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
+        String entCode = ExtJarHelper.getSevInfo().entityInfo.code;
+        ExtJarHelper.getEntityRecordList().forEach(entityRecord -> {
+            String id = EntityRecordUtil.getId(entityRecord);
+            myJdbcTemplate.update("update " + entCode + " t set t.status=?,t.ver=t.ver+1,t.ts=now() where t.id=?", status.toString(), id);
+        });
     }
 }
