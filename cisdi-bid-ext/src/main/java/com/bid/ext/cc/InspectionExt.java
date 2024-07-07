584a6f071f2543d5bb6c78097864c0504bb61aa5
qiufei123456 <8080707+qiufei123456@user.noreply.gitee.com>
2024-07-07 18:50:11 +0800

    巡检的节点扩展：消息推送

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/InspectionExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/InspectionExt.java
@@ -4,22 +4,29 @@ import com.bid.ext.model.CcEarlyWarning;
 import com.bid.ext.model.CcEarlyWarningSetting;
 import com.bid.ext.model.CcPrjMember;
 import com.bid.ext.model.CcQsInspection;
+import com.bid.ext.utils.SysSettingUtil;
 import com.qygly.ext.jar.helper.ExtJarHelper;
 import com.qygly.ext.jar.helper.MyJdbcTemplate;
 import com.qygly.ext.jar.helper.sql.Where;
 import com.qygly.shared.BaseException;
 import com.qygly.shared.ad.entity.EntityInfo;
+import com.qygly.shared.ad.login.LoginInfo;
 import com.qygly.shared.ad.sev.SevInfo;
 import com.qygly.shared.interaction.EntityRecord;
 import com.qygly.shared.util.JdbcMapUtil;
+import com.qygly.shared.util.JsonUtil;
 import com.qygly.shared.util.SharedUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
+@Slf4j
 public class InspectionExt {
 
     /**
@@ -54,9 +61,9 @@ public class InspectionExt {
     public void checkPlanDate() {
         for (EntityRecord entityRecord : ExtJarHelper.getEntityRecordList()) {
             Map<String, Object> valueMap = entityRecord.valueMap;
-            //从
+            // 从
             LocalDate planFr = LocalDate.parse(valueMap.get("PLAN_FR").toString());
-            //到
+            // 到
             LocalDate planTo = LocalDate.parse(valueMap.get("PLAN_TO").toString());
             if (planTo.isBefore(planFr)) {
                 throw new BaseException("计划结束时间早于计划开始时间！");
@@ -117,12 +124,12 @@ public class InspectionExt {
     public void safeEarlyWarning() {
         MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
 
-        //禁令预警查询
+        // 禁令预警查询
         CcEarlyWarningSetting prohibition = CcEarlyWarningSetting.selectOneByWhere(new Where().eq(CcEarlyWarningSetting.Cols.CC_QS_ISSUE_LEVEL_ID, "prohibition"));
         String adUserIds = prohibition.getAdUserIds();
         Integer triggeredWarningIssueCount = prohibition.getTriggeredWarningIssueCount();
 
-        //A类预警查询
+        // A类预警查询
         CcEarlyWarningSetting a = CcEarlyWarningSetting.selectOneByWhere(new Where().eq(CcEarlyWarningSetting.Cols.CC_QS_ISSUE_LEVEL_ID, "A"));
         String aAdUserIds = a.getAdUserIds();
         Integer aTriggeredWarningIssueCount = a.getTriggeredWarningIssueCount();
@@ -161,6 +168,141 @@ public class InspectionExt {
                     ccEarlyWarning.insertById();
                 }
             }
+        }
+    }
+
+
+    /**
+     * 节点扩展。
+     * 在进入节点时，向节点的用户，发送微信模板消息。
+     */
+    public void nodeExt() {
+        for (EntityRecord entityRecord : ExtJarHelper.getEntityRecordList()) {
+            String csCommId = entityRecord.csCommId;
+            List<Map<String, Object>> list = getTaskList(csCommId);
+
+            // 遍历记录，进行发送:
+            list.forEach(item -> {
+                String msg = "流程实例（ID：" + item.get("WF_PROCESS_INSTANCE_ID") + "）的节点实例（ID:" + item.get("WF_NODE_INSTANCE_ID") + "）向用户（ID:" + item.get("user_id") + "，NAME：" + item.get("user_name") + "）发微信模板消息的状态：";
+
+                try {
+                    String userExtraInfo = JdbcMapUtil.getString(item, "user_EXTRA_INFO");
+                    if (SharedUtil.isEmpty(userExtraInfo)) {
+                        log.error(msg + "失败！用户没有EXTRA_INFO，故忽略发送！");
+                    } else {
+                        Map map = JsonUtil.fromJson(userExtraInfo, Map.class);
+                        if (SharedUtil.isEmpty(map.get("openid"))) {
+                            log.error(msg + "失败！用户的EXTRA_INFO没有openid，故忽略发送！");
+                        } else {
+                            String openid = map.get("openid").toString();
+                            String taskId = JdbcMapUtil.getString(item, "task_id");
+                            String viewId = JdbcMapUtil.getString(item, "AD_VIEW_ID");
+                            String pbsNodeName = JdbcMapUtil.getString(item, "pbs_node_name");
+                            String rectificationPeriod = JdbcMapUtil.getString(item, "RECTIFICATION_PERIOD");
+                            String currentStateName = JdbcMapUtil.getString(item, "current_state_name");
+                            sendTemplateMessage(openid, taskId, viewId, csCommId, pbsNodeName, rectificationPeriod, currentStateName);
+                            log.error(msg + "成功。");
+                        }
+                    }
+                } catch (Exception exception) {
+                    log.error(msg + "失败！" + exception.getMessage());
+                }
+            });
+        }
+
+    }
+
+    /**
+     * 获取当前节点的任务（及相关信息）列表。
+     *
+     * @param csCommId
+     * @return
+     */
+    private static List<Map<String, Object>> getTaskList(String csCommId) {
+        String nodeInstId = ExtJarHelper.getNodeInstId();
+
+        // 获取当前
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
+        List<Map<String, Object>> list = myJdbcTemplate.queryForList("select t.id task_id,t.WF_PROCESS_INSTANCE_ID,t.WF_NODE_INSTANCE_ID,n.id node_id,n.code node_code,n.name->>'$.ZH_CN' node_name,t.WF_TASK_TYPE_ID,u.id user_id,u.name->>'$.ZH_CN' user_name,u.EXTRA_INFO user_EXTRA_INFO,state.code current_state_code,state.name->>'$.ZH_CN' current_state_name,n.AD_VIEW_ID,sn.name pbs_node_name,i.RECTIFICATION_PERIOD from wf_task t join ad_user u on t.WF_NODE_INSTANCE_ID=? and t.ad_user_id=u.id join cc_qs_inspection i on i.id=? join cc_qs_current_state state on i.CC_QS_CURRENT_STATE_ID=state.id join wf_node n on t.WF_NODE_ID=n.id join CC_PRJ_STRUCT_NODE sn on sn.id=i.CC_PRJ_STRUCT_NODE_ID", nodeInstId, csCommId);
+        return list;
+    }
+
+    /**
+     * 获取AccessToke。先从reids取，若无则换取并放入redis。
+     *
+     * @return
+     */
+    private String getAccessToken() {
+        StringRedisTemplate stringRedisTemplate = ExtJarHelper.getStringRedisTemplate();
+        String wxAccessToken = stringRedisTemplate.opsForValue().get(REDIS_KEY);
+        if (SharedUtil.notEmpty(wxAccessToken)) {
+            return wxAccessToken;
+        } else {
+            RestTemplate restTemplate = ExtJarHelper.getRestTemplate();
+            ResponseEntity<String> response = restTemplate.getForEntity("https://api.weixin.qq.com/cgi-bin/token?appid=" + APP_ID + "&secret=" + SECRET + "&grant_type=client_credential", String.class);
+            String responseBody = response.getBody();
+
+            Map map = JsonUtil.fromJson(responseBody, Map.class);
+            Object accessToken = map.get("access_token");
+            if (SharedUtil.isEmpty(accessToken)) {
+                throw new BaseException("获取微信AccessToken失败！" + responseBody);
+            } else {
+                // 微信官方表示7200秒失效，我们自行另其6000秒失效，便于更换新的：
+                String accessTokenString = accessToken.toString();
+                stringRedisTemplate.opsForValue().set(REDIS_KEY, accessTokenString, 6000, TimeUnit.SECONDS);
+                return accessTokenString;
+            }
+        }
+    }
+
+    public static final String REDIS_KEY = "WxAccessToken";
+
+    public static final String APP_ID = ((Supplier<String>) () -> "wxe07").get();
+    public static final String SECRET = ((Supplier<String>) () -> "ea81").get();
+    public static final String TEMPLATE_ID = ((Supplier<String>) () -> "iR9").get();
+
+    private void sendTemplateMessage(String openId, String taskId, String viewId, String entityRecordId, String pbsNodeName, String rectificationPeriod, String currentStateName) {
+        LoginInfo loginInfo = ExtJarHelper.getLoginInfo();
+        String currentOrgId = loginInfo.currentOrgInfo.id;
+        String fileDownloadUrl = SysSettingUtil.getValue("FILE_DOWNLOAD_URL");
+        String pageUrl = fileDownloadUrl.replaceAll("/qygly-gateway/qygly-file/downloadCommonFile", "/#");
+
+        Map<String, Object> requestMap = new HashMap<>();
+
+        requestMap.put("touser", openId);
+
+        requestMap.put("template_id", TEMPLATE_ID);
+        requestMap.put("client_msg_id", taskId);
+        String url = pageUrl + "/?type=ACCESS&orgId=" + currentOrgId + "&hideTitleBar=true&hideMenu=true&viewId=" + viewId + "&viewComponent=DETAIL_VIEW&title=%E6%9F%A5%E7%9C%8B%EF%BC%9A%E8%B4%A8%E5%AE%89%E5%B7%A1%E6%A3%80&id=" + entityRecordId;
+        requestMap.put("url", url);
+
+        // 模板详情：
+        // 巡查项目{{thing2.DATA}}
+        // 整改期限{{time10.DATA}}
+        // 处置结果{{thing5.DATA}}
+
+        HashMap<Object, Object> data = new HashMap<>();
+        requestMap.put("data", data);
+
+        HashMap<Object, Object> thing2Map = new HashMap<>();
+        data.put("thing2", thing2Map);
+        thing2Map.put("value", pbsNodeName);
+
+        HashMap<Object, Object> thing10Map = new HashMap<>();
+        data.put("time10", thing10Map);
+        thing10Map.put("value", rectificationPeriod);
+
+        HashMap<Object, Object> thing5Map = new HashMap<>();
+        data.put("thing5", thing5Map);
+        thing5Map.put("value", currentStateName);
+
+        String accessToken = getAccessToken();
+        RestTemplate restTemplate = ExtJarHelper.getRestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=" + accessToken, requestMap, String.class);
+        String responseBody = response.getBody();
+        Map map = JsonUtil.fromJson(responseBody, Map.class);
+        if (map == null || !"ok".equals(map.get("errmsg"))) {
+            throw new BaseException("发送模板消息失败！" + responseBody);
         }
     }
 
