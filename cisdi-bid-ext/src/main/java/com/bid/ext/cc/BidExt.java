0df66b7ada39c6cec7130884270c76eea68ad383
xiamulan <758902836@qq.com>
2024-07-23 11:22:41 +0800

    导入合同工工程量不关联单条数据

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/BidExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/BidExt.java
@@ -165,9 +165,6 @@ public class BidExt {
 //        }
     }
 
-    /**
-     * 导入合同台账
-     */
     public void importContract() {
         Map<String, Object> varMap = ExtJarHelper.getVarMap();
         FlFile flFile = FlFile.selectById(varMap.get("P_CC_ATTACHMENT").toString());
