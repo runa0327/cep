5254b4ef7bd37f09124610ab2b4713806974c1e5
LTM <1537189661@qq.com>
2024-07-30 18:11:41 +0800

    考勤打卡和工人信息base64编码转为图片 扩展开发

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/ImgExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/ImgExt.java
@@ -25,7 +25,7 @@ import java.util.Map;
 
 public class ImgExt {
     /**
-     * base64编码转为图片
+     * 考勤打卡 base64编码转为图片
      */
     public void base64ToImg(){
         MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
@@ -43,6 +43,25 @@ public class ImgExt {
 
     }
 
+    /**
+     * 工人信息 base64编码转为图片
+     */
+    public void userInfoBase64ToImg(){
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
+        EntityRecord entityRecord = ExtJarHelper.getEntityRecordList().get(0);
+        Map<String, Object> valueMap = entityRecord.valueMap;
+        String id = JdbcMapUtil.getString(valueMap, "ID");
+        String RECENT_PHOTO = JdbcMapUtil.getString(valueMap, "RECENT_PHOTO");//近照
+        String userId = ExtJarHelper.getLoginInfo().userInfo.id;
+        String fileId = Crud.from("fl_file").insertData();
+        Map<String, Object> queryForMap = myJdbcTemplate.queryForMap("SELECT `NAME` FROM ad_user WHERE id = ?", userId);
+        String name = JdbcMapUtil.getString(queryForMap, "NAME");
+        String userName = JsonUtil.getCN(name);
+        toImg(fileId,RECENT_PHOTO,userId,userName);
+        Crud.from("CC_WORKER_INFORMATION").where().eq("ID",id).update().set("IMGS",fileId).exec();
+
+    }
+
     public static void main(String[] args) {
 //        String str = "/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDABcQERQRDhcUEhQaGBcbIjklIh8fIkYyNSk5UkhXVVFIUE5bZoNvW2F8Yk5QcptzfIeLkpSSWG2grJ+OqoOPko3/2wBDARgaGiIeIkMlJUONXlBejY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY3/wAARCACaAFEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDB3St6/hSiGRyB3PQE9a7uHR7CJiy2yEkY+bLD8jUlw8WnWMjxRIioMhVXAyenT3q+ZLYz5e5xC2ZgciZSHH8LDGKkAxTnYu5ZjlmOSfUmmSJvjZc4yOoqG7jSsRzjdtQMoPXk0kW2EfO2Sx4xk5p626LJvXI9u1OWGNduF+705PFIq/QRtxyEADADBNQr9oBOeTmrdFNMkiaTaUBU5b07U9HViwU52nB9qdTGhRt3GC3OR6+tMCXJoqD7Of8AntL/AN9UUwO+BrD8S3AAhtwRn77DuOw/rW07rDE0jnCopYnGcAVxFxO00rTTH5nJJ/8Are1SV0GjmlqAzYBOAFHUmqz3spb92do+g5osKxoilrKa4uH6yMPpxR5s3/PWT/vo0iuVmrRWYtzcL0kP480ovLhWBLBvYgY/SgXKzSxS4qgNQkB+eNSPQcVImoRnG5WU/mBVCsXMUVB9ut/7/wD46f8ACigVjrddlWLSpNzFdxAGO/OcfkK4meYsCxOM9B7elbXiSZpdTMTtiOIDA/DJNc3yzcmkUDMzn5iT9TSqlOjXIzUgApNmkUNC0uypAKXFRcuxFspCtTYpCtFwsQEUwrVgrTStUmS0QbaKl20U+YnlOn8T2WLiO6UDbJ8rf7w6fmP5Vy7xlHCngmvR7y2S8tJIHOA44Poex/OvP7uGSG+8qYFXQ4I/GmSLIuxAAKr/ADZq1MR0qE4FSWkNDOKmQk9ai3j1p6nNJlIfTHZh0FOppOKSKZHuc9qcoJIo3LnrUwA27gaq5FhNh9KKk3e9FK4HfgHua5LxSg/teI9/KU/+PGuuyPWuO8Qs51xg+duxdn0x/jmtDNWMmXJc1HtzzU0n3qaKzbNEiLy8VIBjgUtFFykhwUnio3U96lQ/MKdKBSQ2VhHk5qRVA4HWjpSqeadybD9pop2RRRcDuAc9TiuX8TsBq8OP+eI/9Cauk4JBz096w/E0IZLefKqFLKW7k4yB+h/OtnsYLcw35NNpW9aQVizdBQBzyaWmkA9aRRIBzxQ21t21uRUZPGM0q7cYFFguKOaci03pT4+tADsUUuaKBHaDFV9RgNzZPHGAJR80ZwDhhyOv5fjVkI3pS7WyOK3OY4IYIIGeOmSCcfhRWx4gsGgl+1Rg7XJLDPfqf8e/8XQCsbPpWUkbxdxCcUmfY04U4dKm5RGCPQ0vI7Gn5p4ORRcdiFTUyimle9DSBVzQIloql9oeinysXMjrf7Yuyf8AVR/iP/r0v9r3eQfJjpv2O3u0V455No/uPj86HtI7WGSUSSMVXIDtkZFa80Tk9nUvbqULrUJr4sk0ZKrwVj7c8evP+FYzjypNh4q7ZzCOOdwpycYOeM81n7txcNzzUW6nS5O9uiJgM0lRhynXketO8wHvU2KuLUiCot4pfNwKVh3JWIUVCg86TJ+4vb19qQkyfeOB2Hc1IGwOBgDoPSqWhL1HfuPR/wDv2tFQb6KdybI3JDb22dlyS/QiNSP13VVOoSeT5cKr5Wejcg+30zz/AI1SCZPzEt9alY5woHNCilsOVWct2OlllkjzK+5m5+lUD8sn1q7L6DtxVWVfkyOopshCikKKe2KRDkU8VBruM8sUoXFOxRRcLISkZuKU0w0ANopcUUxFulQfNmkNOX7rVRkI7AHkgfWk2gj2NVG5OTzV1P8AVj6UwKgUo5X0qQCmz/68fSniokjWL0ExQRThSGpKGEU09etI5PPNLb8ynPpVJEtjvKairFFOxHMz/9k=";
 //        //转为img
@@ -56,12 +75,13 @@ public class ImgExt {
 //            e.printStackTrace();
 //        }
 //                            set("ORIGIN_FILE_PHYSICAL_LOCATION","/data/qygly/file3/prod/FileStore/"+DateUtil.year(new Date())+"/"+month.format(new Date())+"/"+day.format(new Date())+"/"+id+".png" )
-        System.out.println(DateUtil.year(new Date()));
-        SimpleDateFormat month = new SimpleDateFormat("MM");
-        SimpleDateFormat day = new SimpleDateFormat("dd");
-        System.out.println(month.format(new Date()));
-        System.out.println(day.format(new Date()));
-
+//        System.out.println(DateUtil.year(new Date()));
+//        SimpleDateFormat month = new SimpleDateFormat("MM");
+//        SimpleDateFormat day = new SimpleDateFormat("dd");
+//        System.out.println(month.format(new Date()));
+//        System.out.println(day.format(new Date()));
+        SimpleDateFormat format = new SimpleDateFormat("YYYYMMDDHHSSmm");
+        System.out.println(format.format(new Date()));
     }
 
     public void toImg(String id,String base64Str,String userId,String userName){
@@ -80,7 +100,8 @@ public class ImgExt {
         LocalDate localDate = LocalDate.now();
         String month = String.format("%02d", localDate.getMonthValue());
         String day = String.format("%02d", localDate.getDayOfMonth());
-        String now = DateUtil.now();
+        SimpleDateFormat format = new SimpleDateFormat("YYYYMMDDHHSSmm");
+        String now = format.format(new Date());
         // 解压输出路径
         String outputDir = flPath.getDir() + year + "/" + month + "/" + day + "/" + now+userName;
         // 将Base64字符串解码为字节数组
@@ -92,6 +113,7 @@ public class ImgExt {
             Path path = Paths.get(imagePath);
             long fileSize = Files.size(path);//字节
             Double size = fileSize/ 1024.0;
+            String nowDate = DateUtil.now();
             Crud.from("fl_file").where().eq("ID",id).update()
                     .set("CODE",id)
                     .set("NAME",now+userName)
@@ -99,14 +121,14 @@ public class ImgExt {
                     .set("FL_PATH_ID","0099250247095872688")
                     .set("EXT","png")
                     .set("STATUS","AP")
-                    .set("CRT_DT",now)
+                    .set("CRT_DT",nowDate)
                     .set("CRT_USER_ID",userId)
-                    .set("LAST_MODI_DT",now)
+                    .set("LAST_MODI_DT",nowDate)
                     .set("LAST_MODI_USER_ID",userId)
                     .set("FILE_INLINE_URL","/qygly-gateway/qygly-file/viewImage?fileId="+id+"")
                     .set("FILE_ATTACHMENT_URL","/qygly-gateway/qygly-file/downloadCommonFile?fileId="+id+"")
-                    .set("TS",now)
-                    .set("UPLOAD_DTTM",now)
+                    .set("TS",nowDate)
+                    .set("UPLOAD_DTTM",nowDate)
                     .set("IS_PUBLIC_READ",1 )
                     .set("ORIGIN_FILE_PHYSICAL_LOCATION","/data/qygly/file3/prod/FileStore/"+DateUtil.year(new Date())+"/"+month+"/"+day+"/"+id+".png" )
                     .set("PHYSICAL_LOCATION","/data/qygly/file3/prod/FileStore/"+DateUtil.year(new Date())+"/"+month+"/"+day+"/"+id+".png" )
