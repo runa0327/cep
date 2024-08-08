2a33a29391c907bc9fc04736a0a9b17113587a93
zyx <heiheiheit@foxmail.com>
2024-08-08 16:29:02 +0800

    增加获取设计咨询发起人

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/DesignInquiExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/DesignInquiExt.java
@@ -23,4 +23,32 @@ public class DesignInquiExt {
             }
         }
     }
+
+    /**
+     * 获取设计咨询发起人员
+     */
+    public void getDesignCrtUserId() {
+        for (EntityRecord entityRecord : ExtJarHelper.getEntityRecordList()) {
+            String csCommId = entityRecord.csCommId;
+            CcDesignInqui ccDesignInqui = CcDesignInqui.selectById(csCommId);
+            String crtUserId = ccDesignInqui.getCrtUserId();
+
+            ExtJarHelper.setReturnValue(crtUserId);
+
+            if (crtUserId != null) {
+//                String[] userIds = assignPersonnel.split(",");
+                ArrayList<String> userIdList = new ArrayList<>();
+                userIdList.add(crtUserId);
+                ExtJarHelper.setReturnValue(userIdList);
+            }
+
+
+//            String assignPersonnel = ccDesignInqui.getAssignPersonnel();
+//            if (assignPersonnel != null && !assignPersonnel.isEmpty()) {
+//                String[] userIds = assignPersonnel.split(",");
+//                ArrayList<String> userIdList = new ArrayList<>(Arrays.asList(userIds));
+//                ExtJarHelper.setReturnValue(userIdList);
+//            }
+        }
+    }
 }
