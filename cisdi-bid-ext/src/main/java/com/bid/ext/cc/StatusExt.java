b7aa13e7d6b35c0b7076c20a0c7c5146ad8a9d47
xiamulan <758902836@qq.com>
2024-10-11 14:27:40 +0800

    变更验收状态

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/StatusExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/StatusExt.java
@@ -8,7 +8,6 @@ import com.qygly.shared.interaction.EntityRecord;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 public class StatusExt {
@@ -43,7 +42,7 @@ public class StatusExt {
         changeStatus(STATE_ID);
     }
 
-    private void changeStatus(String STATE_ID){
+    private void changeStatus(String STATE_ID) {
         MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
         SevInfo sevInfo = ExtJarHelper.getSevInfo();
         EntityInfo entityInfo = sevInfo.entityInfo;
@@ -54,7 +53,56 @@ public class StatusExt {
             for (EntityRecord entityRecord : entityRecordList) {
                 String csCommId = entityRecord.csCommId;
 //                Map<String, Object> valueMap = entityRecord.valueMap;
-                int update = myJdbcTemplate.update("update " + entityCode + " t set t.CC_QS_CURRENT_STATE_ID = ? where t.id=?",STATE_ID , csCommId);
+                int update = myJdbcTemplate.update("update " + entityCode + " t set t.CC_QS_CURRENT_STATE_ID = ? where t.id=?", STATE_ID, csCommId);
+                log.info("已更新：{}", update);
+            }
+        }
+    }
+
+    /**
+     * 竣工验收状态变更为“验收申请审批中”
+     */
+    public void changeStatusToApprove() {
+        //待复核
+        String status = "approve";
+        changeAcceptanceStatus(status);
+    }
+
+    /**
+     * 竣工验收状态变更为“验收中”
+     */
+    public void changeStatusToAcceptanceInProgress() {
+        //待复核
+        String status = "acceptance_in_progress";
+        changeAcceptanceStatus(status);
+    }
+
+    /**
+     * 竣工验收状态变更为“验收完成”
+     */
+    public void changeStatusToAccepted() {
+        //待复核
+        String status = "accepted";
+        changeAcceptanceStatus(status);
+    }
+
+    /**
+     * 变更验收状态
+     *
+     * @param status
+     */
+    private void changeAcceptanceStatus(String status) {
+        MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
+        SevInfo sevInfo = ExtJarHelper.getSevInfo();
+        EntityInfo entityInfo = sevInfo.entityInfo;
+        String entityCode = entityInfo.code;
+        List<EntityRecord> entityRecordList = ExtJarHelper.getEntityRecordList();
+
+        if (entityRecordList != null) {
+            for (EntityRecord entityRecord : entityRecordList) {
+                String csCommId = entityRecord.csCommId;
+//                Map<String, Object> valueMap = entityRecord.valueMap;
+                int update = myJdbcTemplate.update("update " + entityCode + " t set t.CC_ACCEPTANCE_STATUS_ID = ? where t.id=?", status, csCommId);
                 log.info("已更新：{}", update);
             }
         }
