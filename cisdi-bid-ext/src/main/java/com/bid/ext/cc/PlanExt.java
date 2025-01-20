4b60a3f44e2625926a75d17809cc51ffcb4c42e0
xiamulan <758902836@qq.com>
2025-01-20 09:55:16 +0800

    引用计划

--- cisdi-bid-ext/src/main/java/com/bid/ext/cc/PlanExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/cc/PlanExt.java
@@ -17,8 +17,7 @@ import com.qygly.shared.util.SharedUtil;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static com.bid.ext.cc.StructNodeExt.replaceIdsAndInsert;
 
@@ -116,7 +115,9 @@ public class PlanExt {
                 ccPrjStructNodeToVersion.insertById();
             }
         }
-
+        InvokeActResult invokeActResult = new InvokeActResult();
+        invokeActResult.reFetchData = true;
+        ExtJarHelper.setReturnValue(invokeActResult);
     }
 
     /**
@@ -127,14 +128,41 @@ public class PlanExt {
         Map<String, Object> varMap = ExtJarHelper.getVarMap();
         // 获取历史版本
         String pCcPrjStructNodeVersion = JdbcMapUtil.getString(varMap, "P_CC_PRJ_STRUCT_NODE_VERSION");
-        String sql = "select * from CC_PRJ_STRUCT_NODE_TO_VERSION t where t.CC_PRJ_STRUCT_NODE_VERSION_ID = ?";
-        List<Map<String, Object>> maps = myJdbcTemplate.queryForList(sql, pCcPrjStructNodeVersion);
+        CcPrjStructNodeVersion ccPrjStructNodeVersion = CcPrjStructNodeVersion.selectById(pCcPrjStructNodeVersion);
+        String ccPrjId = ccPrjStructNodeVersion.getCcPrjId();
+        String ccPrjWbsTypeId = ccPrjStructNodeVersion.getCcPrjWbsTypeId();
+        String version = ccPrjStructNodeVersion.getName();
+
+        List<CcPrjStructNodeVersion> ccPrjStructNodeVersions = CcPrjStructNodeVersion.selectByWhere(new Where().eq(CcPrjStructNodeVersion.Cols.CC_PRJ_ID, ccPrjId).eq(CcPrjStructNodeVersion.Cols.CC_PRJ_WBS_TYPE_ID, ccPrjWbsTypeId));
+        // 使用流操作对version进行排序
+        Optional<CcPrjStructNodeVersion> maxVersionNode = ccPrjStructNodeVersions.stream()
+                .max(Comparator.comparingInt(prjStructNodeVersion -> Integer.parseInt(prjStructNodeVersion.getName().replaceAll("V", ""))));
+        Boolean isNewVersion = false;
+        // 获取最大的version值
+        if (maxVersionNode.isPresent()) {
+            String maxVersion = maxVersionNode.get().getName();
+            if (version.equals(maxVersion)) {
+                isNewVersion = true;
+            }
+        }
+
+        String sql = "select * from cc_prj_struct_node t where t.id = ?";
+        List<CcPrjStructNodeToVersion> ccPrjStructNodeToVersions = CcPrjStructNodeToVersion.selectByWhere(new Where().eq(CcPrjStructNodeToVersion.Cols.CC_PRJ_STRUCT_NODE_VERSION_ID, pCcPrjStructNodeVersion));
+        List<Map<String, Object>> maps = new ArrayList<>();
+        if (!SharedUtil.isEmpty(ccPrjStructNodeToVersions)) {
+            for (CcPrjStructNodeToVersion ccPrjStructNodeToVersion : ccPrjStructNodeToVersions) {
+                String ccPrjStructNodeId = ccPrjStructNodeToVersion.getCcPrjStructNodeId();
+                Map<String, Object> map = myJdbcTemplate.queryForMap(sql, ccPrjStructNodeId);
+                map.put("COPY_FROM_PRJ_STRUCT_NODE_ID", map.get("ID").toString());
+                maps.add(map);
+            }
+        }
         List<Map<String, Object>> list = replaceIdsAndInsert(maps);
         // 序号
         BigDecimal seqNo = BigDecimal.ZERO;
         // 对于每一个模板结构节点，将其作为子节点插入
         for (Map<String, Object> node : list) {
-            insertWbsNode(node, seqNo);
+            insertWbsNode(node, seqNo, isNewVersion);
             seqNo = seqNo.add(BigDecimal.ONE);
         }
         InvokeActResult invokeActResult = new InvokeActResult();
@@ -147,10 +175,14 @@ public class PlanExt {
      *
      * @param nodeData
      */
-    public static void insertWbsNode(Map<String, Object> nodeData, BigDecimal seqNo) {
+    public static void insertWbsNode(Map<String, Object> nodeData, BigDecimal seqNo, Boolean isNewVersion) {
         LoginInfo loginInfo = ExtJarHelper.getLoginInfo();
         String ccPrjId = nodeData.get("CC_PRJ_ID").toString();
         String ccPrjWbsTypeId = nodeData.get("CC_PRJ_WBS_TYPE_ID").toString();
+        String copyFromPrjStructNodeId = null;
+        if (isNewVersion) {
+            copyFromPrjStructNodeId = nodeData.get("COPY_FROM_PRJ_STRUCT_NODE_ID") != null ? nodeData.get("COPY_FROM_PRJ_STRUCT_NODE_ID").toString() : null;
+        }
 
         LocalDate planFr = JdbcMapUtil.getLocalDate(nodeData, "PLAN_FR");
         LocalDate planTo = JdbcMapUtil.getLocalDate(nodeData, "PLAN_TO");
@@ -189,6 +221,7 @@ public class PlanExt {
         ccPrjStructNode.setCcPrjStructNodePid(ccPrjStructNodePid);
 
         ccPrjStructNode.setIsTemplate(false);
+        ccPrjStructNode.setCopyFromPrjStructNodeId(copyFromPrjStructNodeId);
         ccPrjStructNode.insertById();
     }
 
