261774d38502bbe1ecd0e6cd0796892c81f50198
hejialun <867280170@qq.com>
2025-04-19 19:12:20 +0800

    fix:亲笔签签署扩展，回调处理

---
+++ cisdi-bid-job/src/main/java/com/pms/bid/job/mapper/ru/AdRoleMapper.java
@@ -0,0 +1,13 @@
+package com.pms.bid.job.mapper.ru;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pms.bid.job.domain.ru.AdRole;
+import com.pms.bid.job.domain.ru.AdUser;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface AdRoleMapper extends BaseMapper<AdRole> {
+
+
+}
