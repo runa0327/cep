92b7c2ed095055f9327440aaf0719115a28fd824
xiamulan <758902836@qq.com>
2025-06-10 13:57:40 +0800

    全景分享可选择文件夹

--- cisdi-microservice/src/main/java/com/cisdi/cisdipreview/model/PanoData.java
+++ cisdi-microservice/src/main/java/com/cisdi/cisdipreview/model/PanoData.java
@@ -2,9 +2,14 @@ package com.cisdi.cisdipreview.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class PanoData {
-    String shareId;
-    String dataType;
-    String orgId;
+    private String shareId;
+    private String dataType;
+    private String orgId;
+
+    private boolean allFile;
+    private List<String> selectedFolders;
 }
