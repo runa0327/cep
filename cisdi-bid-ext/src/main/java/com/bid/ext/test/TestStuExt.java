bb9fbe0d7d3140aebac95251a861f3d26a045705
qf119ys <qf119ys@163.com>
2025-09-02 16:41:09 +0800

    访问当前数据库及主应用数据库

--- cisdi-bid-ext/src/main/java/com/bid/ext/test/TestStuExt.java
+++ cisdi-bid-ext/src/main/java/com/bid/ext/test/TestStuExt.java
@@ -8,9 +8,14 @@ import com.qygly.shared.util.EntityRecordUtil;
 
 public class TestStuExt {
     public void test() {
+        // 访问当前数据库：
         MyJdbcTemplate myJdbcTemplate = ExtJarHelper.getMyJdbcTemplate();
         for (EntityRecord entityRecord : ExtJarHelper.getEntityRecordList()) {
             myJdbcTemplate.update("update test_stu t set t.ver=t.ver+1,t.ts=now(),t.remark=? where t.id=?", IdUtil.getSnowflakeNextIdStr(), EntityRecordUtil.getId(entityRecord));
         }
+
+        // 访问主应用数据库：
+        MyJdbcTemplate mainMyJdbcTemplate = ExtJarHelper.Main.getMyJdbcTemplate();
+        mainMyJdbcTemplate.update("update ad_user t set t.remark='123' where t.code='admin'");
     }
 }
