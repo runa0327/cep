a73630e5b681aa26251ecab4ad6ecec3828f20e7
qf119ys <qf119ys@163.com>
2025-09-15 09:20:34 +0800

    UrlToOpen、ViewToOpen支持：视图显示模式（页签、弹框、抽屉）、弹出宽度/高度、抽屉宽度

--- demo-ext/src/main/java/demo/ext/test/TestExt.java
+++ demo-ext/src/main/java/demo/ext/test/TestExt.java
@@ -1,9 +1,41 @@
 package demo.ext.test;
 
+import com.qygly.shared.ad.ViewShowMode;
+import com.qygly.shared.ad.ext.UrlToOpen;
+import com.qygly.shared.ad.ext.ViewToOpen;
+import com.qygly.shared.interaction.InvokeActResult;
+
+import java.util.ArrayList;
+
 public class TestExt {
     public void changeVar() {
     }
 
     public void save() {
+    }
+
+    public void f1() {
+        InvokeActResult invokeActResult = new InvokeActResult();
+        invokeActResult.urlToOpenList = new ArrayList<>();
+
+        UrlToOpen urlToOpen = new UrlToOpen();
+        urlToOpen.url = "https://www.baidu.com";
+        urlToOpen.viewShowMode = ViewShowMode.DIALOG;
+        urlToOpen.popupWidth = "500";
+        urlToOpen.popupHeight = "80%";
+
+        invokeActResult.urlToOpenList.add(urlToOpen);
+    }
+
+    public void f2() {
+        InvokeActResult invokeActResult = new InvokeActResult();
+        invokeActResult.viewToOpenList = new ArrayList<>();
+
+        ViewToOpen viewToOpen = new ViewToOpen();
+        viewToOpen.viewId = "视图ID";
+        viewToOpen.viewShowMode = ViewShowMode.DRAWER;
+        viewToOpen.drawerWidth = "30%";
+
+        invokeActResult.viewToOpenList.add(viewToOpen);
     }
 }
